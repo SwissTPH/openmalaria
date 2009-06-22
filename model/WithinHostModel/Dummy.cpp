e2a309f863c09a15abce4b666291f5cbddf548af
diggory.hardy <diggory.hardy@aa1ca794-fd11-11dd-ac65-f74f249cfaac>
2009-06-22 14:50:59 +0000

    Corrected code and tweaked DummyPKPD test - hopefully drug attenuation is right now (although doses may be wrong). Increased pop size to 1000 to get less stochastic results.
    git-svn-id: https://openmalaria.googlecode.com/svn/trunk@272 aa1ca794-fd11-11dd-ac65-f74f249cfaac

--- model/WithinHostModel/Dummy.cpp
+++ model/WithinHostModel/Dummy.cpp
@@ -69,9 +69,6 @@ void DummyWithinHostModel::update (double age) {
   drugProxy->setWeight (120.0 * wtprop[TransmissionModel::getAgeGroup(age)]);
   std::list<DummyInfection>::iterator i;
   for(i=infections.begin(); i != infections.end(); i++){
-    double fact = exp(-drugProxy->getDrugFactor(i->getProteome()));
-    if (fact < 1.0)
-      cout << fact << endl;
     i->multiplyDensity(drugProxy->getDrugFactor(i->getProteome()));
   }
   drugProxy->decayDrugs();
