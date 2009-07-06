94309437e93bbb8aa86c7aa098eea37319f7b284
diggory.hardy <diggory.hardy@aa1ca794-fd11-11dd-ac65-f74f249cfaac>
2009-07-06 13:40:45 +0000

    Included the case management decision-tree converter in the simulator.
    git-svn-id: https://openmalaria.googlecode.com/svn/trunk@290 aa1ca794-fd11-11dd-ac65-f74f249cfaac

---
+++ util/CaseManagementTree/CM.java
@@ -0,0 +1,204 @@
+/*
+ This file is part of OpenMalaria.
+ 
+ Copyright (C) 2005-2009 Swiss Tropical Institute and Liverpool School Of Tropical Medicine
+ 
+ OpenMalaria is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or (at
+ your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class CM {
+
+    DocumentBuilder _builder;
+
+    static String _outfileName = "cm_out.xml";
+
+    public CM() {
+        try {
+            _builder = DocumentBuilderFactory.newInstance()
+                    .newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void processNodes(Element parent, Map<String, Double> outcomes,
+            Double p) {
+        Element childElement = (Element) parent.getChildNodes().item(1);
+        if (childElement.getNodeName().equals("decision")) {
+
+            String outcome = childElement.getTextContent();
+            if (outcomes.containsKey(outcome)) {
+                outcomes.put(outcome, outcomes.get(outcome) + p);
+            } else {
+                outcomes.put(outcome, p);
+            }
+            return;
+        }
+
+        NodeList nodeList = parent.getChildNodes();
+        double pc = 0.0;
+        for (int index = 0; index < nodeList.getLength(); index++) {
+            if (nodeList.item(index).getNodeType() != Node.ELEMENT_NODE) {
+                continue;
+            }
+            Element node = (Element) nodeList.item(index);
+            Double pNode = Double.parseDouble(node.getAttribute("p"));
+            processNodes(node, outcomes, p * pNode);
+            pc = pc
+                    + Double.parseDouble(((Element) nodeList.item(index))
+                            .getAttribute("p"));
+        }
+        if (!(pc == 1.0)) {
+            System.out.println("Parental node '"
+                    + parent.getAttribute("decision")
+                    + "' has at least one child with a wrong probability");
+        }
+        // System.out.println("prob. check of parent " +
+        // parent.getAttribute("decision") + " = " + pc);
+    }
+
+    public double sumProbabilities(Element entryPoint) {
+        double p = 0.0;
+        NodeList decisionList = entryPoint.getChildNodes();
+        for (int dcindex = 0; dcindex < decisionList.getLength(); dcindex++) {
+            Element decision = (Element) decisionList.item(dcindex);
+            p = p + Double.parseDouble(decision.getAttribute("p"));
+        }
+        return p;
+    }
+
+    public void checkUnitProbability(File outfile) throws Exception {
+        Element caseManagements = _builder.parse(outfile).getDocumentElement();
+        NodeList cmList = caseManagements
+                .getElementsByTagName("caseManagement");
+        for (int cmindex = 0; cmindex < cmList.getLength(); cmindex++) {
+            NodeList cm = (NodeList) cmList.item(cmindex);
+            for (int epindex = 0; epindex < cm.getLength(); epindex++) {
+                Element ep = (Element) cm.item(epindex);
+                if ((ep.getTagName() == "uc1") || (ep.getTagName() == "uc2")
+                        || (ep.getTagName() == "sev")
+                        || (ep.getTagName() == "nmfwithparasites")
+                        || (ep.getTagName() == "nmfwithoutparasites")) {
+                    double totalProb = sumProbabilities(ep);
+                    if (!(totalProb == 1.0)) {
+                        System.out
+                                .println("Probabilities in Casemanagement number "
+                                        + (cmindex + 1)
+                                        + " entry point <"
+                                        + ep.getTagName()
+                                        + "> add up to "
+                                        + totalProb + " instead of 1.0");
+                    }
+                }
+            }
+        }
+    }
+
+    public void translate(File file) throws Exception {
+        Element ageDepDecisionTrees = _builder.parse(file).getDocumentElement();
+        Document hsDoc = _builder.newDocument();
+        Element newRoot = hsDoc.createElement("caseManagements");
+        hsDoc.appendChild(newRoot);
+        newRoot.setAttribute("name", "test");
+        NodeList dtList = ageDepDecisionTrees
+                .getElementsByTagName("decisionTrees");
+        String minAY = "0.0";
+        for (int dtindex = 0; dtindex < dtList.getLength(); dtindex++) {
+            Element decisionTree = ((Element) dtList.item(dtindex));
+            Element cm = hsDoc.createElement("caseManagement");
+            newRoot.appendChild(cm);
+            NodeList treeList = decisionTree.getElementsByTagName("tree");
+            cm.setAttribute("minAgeYrs", minAY);
+            cm
+                    .setAttribute("maxAgeYrs", decisionTree
+                            .getAttribute("maxAgeYrs"));
+            minAY = decisionTree.getAttribute("maxAgeYrs").toString();
+            for (int index = 0; index < treeList.getLength(); index++) {
+                Map<String, Double> outcomeMap = new HashMap<String, Double>();
+                Element tree = (Element) treeList.item(index);
+                String entryPoint = tree.getAttribute("entryPoint");
+                Element ep = hsDoc.createElement(entryPoint);
+                cm.appendChild(ep);
+                processNodes(tree, outcomeMap, 1.0);
+                for (Iterator<String> outcomeiterator = outcomeMap.keySet()
+                        .iterator(); outcomeiterator.hasNext();) {
+                    String ocname = outcomeiterator.next();
+                    Element oc = hsDoc.createElement("endPoint");
+                    // oc.setAttribute("p",
+                    // Double.toString(Math.round(outcomeMap
+                    // .get(ocname) * 100.0) / 100.0));
+                    oc.setAttribute("p", Double
+                            .toString(outcomeMap.get(ocname)));
+                    oc.setAttribute("decision", ocname);
+                    ep.appendChild(oc);
+                }
+            }
+
+            Node outcomes = ageDepDecisionTrees.getElementsByTagName(
+                    "decisions").item(0);
+            Element ocs = (Element) hsDoc.importNode(outcomes, true);
+            cm.appendChild(ocs);
+        }
+        Source source = new DOMSource(hsDoc);
+        // Prepare the output file
+        File outFile = new File(_outfileName);
+        OutputStream os = new FileOutputStream(outFile);
+        Result result = new StreamResult(os);
+        // Write the DOM document to the file
+        Transformer xformer = TransformerFactory.newInstance().newTransformer();
+        xformer.transform(source, result);
+    }
+
+    public static void main(String[] args) {
+        CM cm = new CM();
+        try {
+            cm.translate(new File("cm.xml"));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            cm.checkUnitProbability(new File(_outfileName));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("\nDone");
+    }
+}
---
+++ util/CaseManagementTree/CaseManagementTree.d
@@ -0,0 +1,342 @@
+/*
+ This file is part of OpenMalaria.
+ 
+ Copyright (C) 2005-2009 Swiss Tropical Institute and Liverpool School Of Tropical Medicine
+ 
+ OpenMalaria is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or (at
+ your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/******************************************************************************
+ * This is a small program to translate VC's case management trees into a
+ * more compact list of outcomes (plus whatever data is needed from the tree).
+ * 
+ * It's written in D because I (DH) find this the easiest way to program this.
+ * See http://www.dsource.org/projects/tango/wiki/TopicInstallTango for setup
+ * instructions. I use dsss for building; run: dsss build
+ * 
+ * API reference:
+ * http://www.dsource.org/projects/tango/docs/current/
+ *****************************************************************************/
+module CaseManagementTree;
+
+import tango.io.device.File;
+import tango.io.Stdout;
+import tango.util.log.Log;
+import tango.util.log.Config;
+import tango.text.xml.Document;
+import tango.text.xml.DocPrinter;
+import tango.math.Math;
+import tango.math.IEEE;
+import Util=tango.text.Util;
+import Float=tango.text.convert.Float;
+import Int=tango.text.convert.Integer;
+
+import Clinical.DecisionEnums;
+
+private Logger log;
+static this() {
+    log = Log.lookup ("CaseManagementTree");
+}
+/+
+/** Possiblity to convert to a smaller representation and check for data-entry errors.
+ *
+ * Possibly store in files:
+ * List of leaf nodes, with attributes like testing method, hospital care or not, delay, treatment plan (small list)
+ * Replace decisions list with treatments list, which allow various drug prescription methods or clearing all parasites.
+ */
+
+struct Branch {
+    int digit;		// digit of decision id adjusted
+    double[] nodeProbs;	// probabilities of each outcome; sum(nodeProbs) must add to 1.0
+}
+struct BranchSet {
+    /** Validates this conforms to other branches contained, and adds in to
+     * set if probabilities are different from existing members. Returns a
+     * reference to the new or existing branch (with same probabilities). */
+    Branch* include (Branch b, char[][] bNodeNames, char[] name) {
+	with (branches[0]) {	// a branch must exist; we only need to check against one
+	    // check format is the same
+	    bool valid = true;
+	    if (digit != b.digit) valid = false;
+	    if (nodeNames.length != bNodeNames.length) valid = false;
+	    // extra node vals/IDs are allowed, so only check those present in both
+	    for (int i = min(nodeNames.length, bNodeNames.length); i >= 0; --i)
+		if (nodeNames[i] != bNodeNames[i]) valid = false;
+	    if (!valid)
+		throw new Exception ("Branch "~name~" has differing formats");
+	}
+	foreach (ref existing; branches) {
+	    if (existing.nodeProbs.length != b.nodeProbs.length) continue;
+	    for (int i = b.nodeProbs.length; i >= 0; --i)
+		if (feqrel (existing.nodeProbs[i], b.nodeProbs[i]) < real.mant_dig-1) continue;
+	    return &existing;
+	}
+	branches ~= b;
+	return &branches[$-1];
+    }
+    
+    Branch[] branches;
+    char[][] nodeNames;	// string value of all outcomes (e.g. "good")
+}
+BranchSet[char[]] branches;	// set of branch sets, with key branch name (e.g. "quality")
++/
+
+/// This is for filtering out the information we want to pass to the simulator
+const DecisionEnums MASK = DecisionEnums.TEST_RDT | DecisionEnums.DRUG_MASK | DecisionEnums.QUALITY_MASK | DecisionEnums.ADHERENCE_MASK | DecisionEnums.MANAGEMENT_MASK | DecisionEnums.TSDELAY_MASK;
+
+int main(char[][] args) {
+    if (args.length < 2 || args.length > 3) {
+	log.fatal ("Usage: {} infile.xml [outfile.xml]", args[0]);
+	return 1;
+    }
+    char[] inFile = args[1];
+    log.info ("Reading file {}", inFile);
+    
+    auto inDoc = new Document!(char), outDoc = new Document!(char);
+    inDoc.parse (cast(char[])File.get (inFile));
+    outDoc.header;
+    
+    auto root = inDoc.elements;
+    assert (root.name == "agedependentDecisionTrees");
+    auto caseManagements = outDoc.tree.element (null, "caseManagements");
+    caseManagements.attribute (null, "name", "NoName");
+    
+    foreach (decisionTrees; root.query["decisionTrees"].dup) {
+	auto caseManagement = caseManagements.element (null, "caseManagement");
+	caseManagement.attribute (null, "maxAgeYrs", decisionTrees.query.attribute("maxAgeYrs").nodes[0].value);
+	foreach (tree; decisionTrees.query["tree"].dup) {
+	    auto caseBranch = caseManagement.element (null, tree.query.attribute("entryPoint").nodes[0].value);
+	    
+	    double[uint] leaves;	// end points of branches
+	    
+	    // recurse on "branch"
+	    uint endPoints = recurseLeaves (tree, leaves);
+	    
+	    double prob = 0.0;
+	    foreach (p; leaves)
+		prob += p;
+	    if (feqrel (prob, 1.0) < double.mant_dig-2)	// test prop approx. eq. 1.0
+		log.warn ("{} leaves from {} end-points (total probability: {})", leaves.length, endPoints, prob);
+	    
+	    foreach (id,p; leaves) {
+		caseBranch.element (null, "endPoint")
+		    .attribute (null, "decision", Int.toString (id, "x#"))
+		    .attribute (null, "p", Float.toString (p, double.dig));
+	    }
+	}
+	
+	/++ This was intended to be code to read decision stuff - but we
+	 + already have enough data to generate this!
+	 + Don't even generate it here; do it in the simulator from the id.
+	---
+	struct DecData {
+	    char[] abbrev;
+	    enum { POOR, SUFFICIENT, GOOD } qty;
+	    int time;
+	}
+	DecData[int] inDecData;
+	auto inDecisions = decisionTrees.query["decisions"].nodes[0];
+	foreach (decision; inDecisions.query["decision"].dup) {
+	    int id = Int.toInt (decision.query.attribute("id"));
+	    assert ((id in inDecData) is null);	// don't want id listed twice
+	    auto medicates = decision.query["medicate"];
+	    assert (mecicates.count == 1, "code intended to translate from type, simple quantity and time to dosage details");
+	    auto med = medicates.nodes[0];
+	    DecData dd;
+	    dd.abbrev = med.query.attribute("name").nodes[0].value;
+	    char[] qty = med.query.attribute("qty").nodes[0].value;
+	    if (qty == "good")			dd.qty = GOOD;
+	    else if (qty == "sufficient")	dd.qty = SUFFICIENT;
+	    else if (qty == "poor")		dd.qty = POOR;
+	    else throw new Exception("Expected qty to be good/sufficient/poor");
+	    dd.time = med.query.attribute("time").nodes[0].value;
+	    inDecData[id] = 
+	}
+	---
+	+/
+    }
+    
+    auto printer = new DocPrinter!(char);
+    printer.indent (2);
+    if (args.length == 3) {
+	File.set (args[2], printer(outDoc));
+	log.info ("Written file {}", args[2]);
+    } else
+	Stdout (printer(outDoc));
+    
+    // Idea was to reformat, but it won't strip old whitespace
+    //File.set ("inputCopy.xml", printer(inDoc));
+    
+    return 0;
+}
+
+uint recurseLeaves (Document!(char).Node node, ref double[uint] leaves, DecisionEnums inFlags = DecisionEnums.NONE, double inProb = 1.0) {
+    auto query = node.query["branch"];
+    if (query.count) {
+	uint endPoints = 0;
+	double cumProb = 0.0;
+	foreach (child; query.dup) {
+	    DecisionEnums flags = inFlags;
+	    char[] decis = child.query.attribute("decision").nodes[0].value;
+	    double prob = Float.toFloat(child.query.attribute("p").nodes[0].value);
+	    cumProb += prob;
+	    prob *= inProb;
+	    char[] end;
+	    if (prependedBy (decis, "tested:", end)) {
+		if (end == "no") {
+		} else if (end == "microscopy") {
+		    flags |= DecisionEnums.TEST_MICROSCOPY;
+		} else if (end == "RDT") {
+		    flags |= DecisionEnums.TEST_RDT;
+		} else
+		    Stdout.format ("unknown - {}", decis).newline;
+	    } else if (prependedBy (decis, "result:", end)) {
+		if (end == "true positive") {
+		    flags |= DecisionEnums.RESULT_TRUE_POS;
+		} else if (end == "false negative") {
+		    flags |= DecisionEnums.RESULT_FALSE_NEG;
+		} else if (end == "false positive") {
+		    flags |= DecisionEnums.RESULT_FALSE_POS;
+		} else if (end == "true negative") {
+		    flags |= DecisionEnums.RESULT_TRUE_NEG;
+		} else if (end == "positive") {
+		    flags |= DecisionEnums.RESULT_FALSE;
+		} else if (end == "negative") {
+		    flags |= DecisionEnums.RESULT_TRUE;
+		} else
+		    Stdout.format ("unknown - {}", decis).newline;
+	    } else if (prependedBy (decis, "drug:", end)) {
+		if (end == "SP") {
+		    flags |= DecisionEnums.DRUG_SP;
+		} else if (end == "AL") {
+		    flags |= DecisionEnums.DRUG_AL;
+		} else if (end == "no antimalarial") {
+		    flags |= DecisionEnums.DRUG_NO_AM;
+		} else
+		    Stdout.format ("unknown - {}", decis).newline;
+	    } else if (prependedBy (decis, "quality:", end)) {
+		if (end == "good") {
+		    flags |= DecisionEnums.QUALITY_GOOD;
+		} else if (end == "bad") {
+		    flags |= DecisionEnums.QUALITY_BAD;
+		} else
+		    Stdout.format ("unknown - {}", decis).newline;
+	    } else if (prependedBy (decis, "adherence:", end)) {
+		if (end == "good") {
+		    flags |= DecisionEnums.ADHERENCE_GOOD;
+		} else if (end == "bad") {
+		    flags |= DecisionEnums.ADHERENCE_BAD;
+		} else
+		    Stdout.format ("unknown - {}", decis).newline;
+	    } else if (prependedBy (decis, "time:", end)) {
+		int val = int.min;
+		if (end.length == 1)
+		    val = end[0] - '0';
+		if (val >= 0 || val <= 15) {
+		    flags |= (val << DecisionEnums.TSDELAY_LSHIFT);
+		} else
+		    Stdout.format ("unknown - {}", decis).newline;
+	    } else if (prependedBy (decis, "treatment:", end)) {
+		if (end == "no antimalarial") {
+		    flags |= DecisionEnums.TREATMENT_NO_AM;
+		} else if (end == "pre-referral only") {
+		    flags |= DecisionEnums.TREATMENT_PREREF;
+		} else {
+		    flags |= DecisionEnums.DRUG_QN;
+		    flags |= DecisionEnums.ADHERENCE_GOOD;
+		    if (end == "parenteral") {
+			flags |= DecisionEnums.TREATMENT_PARENTAL;
+		    } else if (end == "delayed referral") {
+			flags |= DecisionEnums.TREATMENT_DELREF | (1 << DecisionEnums.TSDELAY_LSHIFT);
+		    } else if (end == "pre-referral and immediate referral") {
+			flags |= DecisionEnums.TREATMENT_PREREF_IMMREF;
+		    } else if (end == "pre-referral and delayed referral") {
+			flags |= DecisionEnums.TREATMENT_PREREF_DELREF | (1 << DecisionEnums.TSDELAY_LSHIFT);
+		    } else
+			log.error ("unknown: {}", decis);
+		}
+	    } else if (prependedBy (decis, "management:", end)) {
+		if (end == "good") {
+		    flags |= DecisionEnums.MANAGEMENT_GOOD;
+		} else if (end == "bad") {
+		    flags |= DecisionEnums.MANAGEMENT_BAD;
+		} else
+		    log.error ("unknown: {}", decis);
+	    } else {
+		log.error ("unknown: {}", decis);
+		continue;
+	    }
+	    endPoints += recurseLeaves (child, leaves, flags, prob);
+	}
+	if (feqrel (cumProb, 1.0) < double.mant_dig-2) {
+	    log.warn ("probabilities of branch [{}] add up to {}",
+		node.name == "branch" ?
+		    node.query.attribute("decision").nodes[0].value :
+		    node.query.attribute("entryPoint").nodes[0].value,
+		cumProb);
+	}
+	return endPoints;
+    } else {
+	int maskedFlags = MASK & inFlags;
+	query = node.query["decision"];
+	assert (query.count == 1);
+	char[] id = query.nodes[0].value;
+	if (id == "1") {
+	    //Stdout.format ("{:x}\tno treatment", inFlags).newline;
+	    double* pLeaf = maskedFlags in leaves;
+	    if (pLeaf) {
+		*pLeaf += inProb;
+	    } else
+		leaves[maskedFlags] = inProb;
+	} else if (id.length == 5
+	    && id[0] == '0'
+	    && (id[1]-'0'+1) == filterRShift!(DecisionEnums.DRUG_MASK) (inFlags)
+	    && ((id[1]=='2') ? ((id[2]-'0') == filterRShift!(DecisionEnums.MANAGEMENT_MASK) (inFlags))
+			     : ((id[2]-'0') == filterRShift!(DecisionEnums.QUALITY_MASK) (inFlags)))
+	    && (id[3]-'0') == filterRShift!(DecisionEnums.ADHERENCE_MASK) (inFlags)
+	    && (id[4]-'0') == filterRShift!(DecisionEnums.TSDELAY_MASK) (inFlags)) {
+	    double* pLeaf = maskedFlags in leaves;
+	    if (pLeaf) {
+		*pLeaf += inProb;
+	    } else
+		leaves[maskedFlags] = inProb;
+	} else
+	    Stdout.format ("0x{:x}\tinvalid id: {}", inFlags, id).newline;
+	
+	return 1;
+    }
+}
+
+bool prependedBy (char[] source, char[] match, out char[] end) {
+    if (source.length >= match.length &&
+	source[0..match.length] == match) {
+	end = Util.triml(source[match.length..$]);
+	return true;
+    }
+    return false;
+}
+
+/** Take val & mask and right-shift by the number of binary zeros less-
+ * significant than the first one in mask.
+ * 
+ * E.g. filterRShift!(0xF0) (0x40) == 0x4. */
+uint filterRShift(DecisionEnums mask, uint rShift = 0) (uint val) {
+    static assert (rShift < uint.sizeof * 8);
+    static if ((mask >> rShift) & 1) {
+	return (val & mask) >> rShift;
+    } else {
+	return filterRShift!(mask, rShift+1) (val);
+    }
+}
