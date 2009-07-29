2f0c2ca44a51a5f9ddee24d0ad5555a2223169ab
diggory.hardy <diggory.hardy@aa1ca794-fd11-11dd-ac65-f74f249cfaac>
2009-07-29 09:54:26 +0000

    Made the compareOutputsFloat.py script compare values with relative precision, plus other changes/fixes.
    git-svn-id: https://openmalaria.googlecode.com/svn/trunk@367 aa1ca794-fd11-11dd-ac65-f74f249cfaac

--- test/compareOutputsFloat.py
+++ test/compareOutputsFloat.py
@@ -30,17 +30,25 @@ def main(*args):
     loc_id2=line_items2[2]
     value2=float(line_items2[3])
     
-    if((loc_id1!=loc_id2) or (h_id1!=h_id2) or (math.fabs(value1 - value2) > 0.000001)):
-      print (str(line_count)+ " " +line_items1[1]+ " " +line_items1[2]+ " " +line_items1[3])
-      print (str(line_count)+ " " +line_items2[1]+ " " +line_items2[2]+ " " +line_items2[3])
+    if (loc_id1!=loc_id2) or (h_id1!=h_id2):
+      print "Different summary outputs {0}:".format(line_count)
+      print '-',line1,
+      print '+',line2,
+      return 2
+    
+    # Compare with relative precision. Although openMalaria normally rounds it's output to less decimal places than this...
+    if math.fabs(value1 - value2) > 0.0000001 * max(math.fabs(value1),math.fabs(value2)):
+      print "Different values output {0}".format(line_count)
+      print '-',line1,
+      print '+',line2,
       for j in range(0, pre_lines-1):
-	print (prev_lines1[j])
-	print (prev_lines2[j])
+	print '-',prev_lines1[j],
+	print '+',prev_lines2[j],
       return 1
     
     if(line_count % 100000 == 0):
       print (line_count)
-    for i in range(1, pre_lines-1):
+    for i in range(pre_lines-1, 0, -1):
       prev_lines1[i]=prev_lines1[i-1]
       prev_lines2[i]=prev_lines2[i-1]
     prev_lines1[0]=line1
