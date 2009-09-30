433028029a695de02ee60962658fe4f9a69cc1a3
diggory.hardy <diggory.hardy@aa1ca794-fd11-11dd-ac65-f74f249cfaac>
2009-09-30 14:36:44 +0000

    Improvements to the compareOutputsFloat.py script.
    git-svn-id: https://openmalaria.googlecode.com/svn/trunk@493 aa1ca794-fd11-11dd-ac65-f74f249cfaac

--- test/compareOutputsFloat.py
+++ test/compareOutputsFloat.py
@@ -5,6 +5,8 @@ import string
 import math
 import unittest
 
+totalRelDiff = 0.0
+
 # Could use math.isinf, but it's not present in python 2.5
 def isinf (a):
     return (a-a) != 0
@@ -22,10 +24,14 @@ def isinf (a):
 # might lead to confusing outcomes.
 def approx_equal (a,b, relPrecision, absPrecision):
     if isinf(a) or isinf(b):
-        return False
-    tolerance = relPrecision * max(math.fabs(a),math.fabs(b))
-    tolerance = max(tolerance, absPrecision)
-    return (math.fabs(a-b) < tolerance)
+        relDiff = 1e1000-1e1000
+    else:
+        tolerance = relPrecision * max(math.fabs(a),math.fabs(b))
+        tolerance = max(tolerance, absPrecision)
+        relDiff = math.fabs(a-b) / tolerance
+    global totalRelDiff
+    totalRelDiff += relDiff
+    return (relDiff < 1.0)
 
 def approx_equal_6 (a,b):
     return approx_equal (a, b, 1e-6, 1e-6)
@@ -66,60 +72,74 @@ class TestApproxEqual (unittest.TestCase):
 
 
 def main(*args):
-  if (4!=len(args)):
-    print "Usage: compareLogs.py logfile1 logfile2 n_prev_lines"
-    return 1
-  
-  print "Comparing "+args[1]+" with "+args[2]
-  file1=open(args[1], 'r')
-  file2=open(args[2], 'r')
-  pre_lines=int(args[3])+1
-  line_count=0
-  prev_lines1=[""]*pre_lines
-  prev_lines2=[""]*pre_lines
-  for line1 in file1:
-    line_count+=1
+    maxDiffsToPrint=6
+    if (len(args) == 4):
+        maxDiffsToPrint=int(args[3])
+    elif (len(args) == 3):
+        pass
+    else:
+        print "Usage: compareLogs.py logfile1 logfile2 [max different lines to print]"
+        return 1
     
-    line_items1=string.split(line1)
-    h_id1=int(line_items1[1])
-    loc_id1=line_items1[2]
-    value1=float(line_items1[3])
+    print "Comparing "+args[1]+" with "+args[2]
+    file1=open(args[1], 'r')
+    file2=open(args[2], 'r')
+    line_count=0
+    numDiffs=0
+    perMeasureNumDiff = dict()
+    perMeasureDiffSum = dict()
+    perMeasureDiffAbsSum = dict()
+    for line1 in file1:
+        line_count+=1
+        
+        line_items1=string.split(line1)
+        h_id1=int(line_items1[1])
+        loc_id1=line_items1[2]
+        value1=float(line_items1[3])
+        
+        line2=file2.readline()
+        line_items2=string.split(line2)
+        h_id2=int(line_items2[1])
+        loc_id2=line_items2[2]
+        value2=float(line_items2[3])
+        
+        if (loc_id1!=loc_id2) or (h_id1!=h_id2):
+            print "Different summary outputs {0}:".format(line_count)
+            print '-',line1,
+            print '+',line2,
+            return 2
+        
+        # Compare with relative precision.
+        if not approx_equal_6 (value1, value2):
+            numDiffs += 1
+            perMeasureNumDiff[h_id1] = perMeasureNumDiff.get(h_id1,0) + 1;
+            if (numDiffs <= maxDiffsToPrint):
+                print "line {0:>5}, survey {1:>3}, age group {2:>3}, measure {3:>3}:{4:>12.5f} ->{5:>12.5f}".format(line_count,line_items1[0],loc_id1,h_id1,value1,value2)
+                if (numDiffs == maxDiffsToPrint):
+                    print "[won't print any more line-by-line diffs]"
+        
+        # Sum up total difference per measure
+        perMeasureDiffSum[h_id1]    = perMeasureDiffSum.get(h_id1,0.0)    + value2 - value1
+        perMeasureDiffAbsSum[h_id1] = perMeasureDiffAbsSum.get(h_id1,0.0) + math.fabs(value2-value1)
+        
+        if(line_count % 100000 == 0):
+            print (line_count)
     
-    line2=file2.readline()
-    line_items2=string.split(line2)
-    h_id2=int(line_items2[1])
-    loc_id2=line_items2[2]
-    value2=float(line_items2[3])
+    if (file2.readline() != ""):
+        print "file {0} has more lines than {1}".format(args[2],args[1])
+        return 3
     
-    if (loc_id1!=loc_id2) or (h_id1!=h_id2):
-      print "Different summary outputs {0}:".format(line_count)
-      print '-',line1,
-      print '+',line2,
-      return 2
+    for (measure,val) in perMeasureDiffAbsSum.iteritems():
+        if val > 1e-6:
+            diff=perMeasureDiffSum[measure]
+            print "Diff sum for measure {0: >3}:{1: >12.5f}\tabs: {2: >12.5f}\t(ratio: {3: >9.5f}; from {4:>3} diffs)".format(measure,diff,val,diff/val,perMeasureNumDiff.get(measure,0))
     
-    # Compare with relative precision.
-    if not approx_equal_6 (value1, value2):
-      print "Different values output on line {0}".format(line_count)
-      print '-',line1,
-      print '+',line2,
-      for j in range(0, pre_lines-1):
-        print '-',prev_lines1[j],
-        print '+',prev_lines2[j],
-      return 1
-    
-    if(line_count % 100000 == 0):
-      print (line_count)
-    for i in range(pre_lines-1, 0, -1):
-      prev_lines1[i]=prev_lines1[i-1]
-      prev_lines2[i]=prev_lines2[i-1]
-    prev_lines1[0]=line1
-    prev_lines2[0]=line2
-  
-  if (file2.readline() != ""):
-    print "file {0} has more lines than {1}".format(args[2],args[1])
-    return 3
-  print "No differences, ok..."
-  return 0
+    if numDiffs == 0:
+        print "No significant differences (total relative diff: {0}), ok...".format(totalRelDiff)
+    else:
+        print "{0} significant differences (total relative diff: {1})!".format(numDiffs,totalRelDiff)
+        return 1
+    return 0
 
 if __name__ == '__main__':
     #uncomment to run unittests:
