20678d1215398946f532db8f24d6036239796ec9
diggory.hardy <diggory.hardy@aa1ca794-fd11-11dd-ac65-f74f249cfaac>
2011-01-25 14:10:25 +0000

    New version number for schema translator.
    git-svn-id: https://openmalaria.googlecode.com/svn/trunk@1624 aa1ca794-fd11-11dd-ac65-f74f249cfaac

--- util/SchemaTranslator/SchemaTranslator.java
+++ util/SchemaTranslator/SchemaTranslator.java
@@ -45,7 +45,7 @@ public class SchemaTranslator {
     Document scenarioDocument;
     Element scenarioElement;
 
-    static final int CURRENT_VERSION = 24;
+    static final int CURRENT_VERSION = 25;
 
     private static int _required_version = CURRENT_VERSION;
     private static boolean latestSchema = false;
