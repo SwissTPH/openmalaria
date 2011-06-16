4591ad64044104031f81d1188f7cbef64baa7434
diggory.hardy <diggory.hardy@aa1ca794-fd11-11dd-ac65-f74f249cfaac>
2011-06-16 13:04:50 +0000

    Corrected/improved documentation of limits of ITN parameters.
    git-svn-id: https://openmalaria.googlecode.com/svn/trunk@1886 aa1ca794-fd11-11dd-ac65-f74f249cfaac

--- model/Transmission/Vector/ITN.cpp
+++ model/Transmission/Vector/ITN.cpp
@@ -77,9 +77,12 @@ void ITNAnophelesParams::RelativeAttractiveness::init(const scnXml::ITNDeterrenc
     respectively, with h and p defined as: h=exp(-holeIndex*holeScalingFactor),
     p=1−exp(-insecticideContent*insecticideScalingFactor).
     
-    h and p will always be in the range [0,1], so we need log(HF) ≤ 0,
-    log(PF) ≤ 0 and log(HF)+log(PF)+log(IF) = log(HF×PF×IF) ≤ 0.
-    This implies that we need HF∈(0,1], PF∈(0,1] and HF×PF×IF∈(0,1].
+    h and p will always be in the range [0,1], so the following limits are
+    sufficient to keep the relative availability in the range [0,1]:
+    log(HF) ≤ 0, log(PF) ≤ 0 and log(HF)+log(PF)+log(IF) = log(HF×PF×IF) ≤ 0,
+    or equivalently HF∈(0,1], PF∈(0,1] and HF×PF×IF∈(0,1].
+    Weaker limits would not be sufficient, as with the argument for the limits
+    of killing effect arguments below.
     */
     if( !( HF > 0.0 && HF <= 1.0 && PF > 0.0 && PF <= 1.0 &&
             HF*PF*IF > 0.0 && HF*PF*IF <= 1.0 ) ){
@@ -115,20 +118,35 @@ void ITNAnophelesParams::SurvivalFactor::init(const scnXml::ITNKillingEffect& el
     }
     
     /* We want the calculated survival factor (1−K)/(1−BF) to be in the range
-    [0,1] where K is the killing factor: K=BF+HF*h+PF*p+IF*h*p, with h and p
-    defined as: h=exp(-holeIndex*holeScalingFactor),
-    p=1−exp(-insecticideContent*insecticideScalingFactor).
+    [0,1] where K is the killing factor: K=BF+HF×h+PF×p+IF×h×p, with h and p
+    defined as: h=exp(-holeIndex×holeScalingFactor),
+    p=1−exp(-insecticideContent×insecticideScalingFactor).
     
-    Since 1−BF > 0 we need 1−K ≥ 0 or equivalently BF+HF*h+PF*p+IF*h*p ≤ 1.
-    Since holeIndex*holeScaling and insecticideContent*insecticideScaling are
-    both non-negative, both h and p are in the range (0,1]. Considering
-    extreme values for the exponentials and realising that their product is
-    no greater than either alone leads us to the following requirements:
-    BF+HF ≤ 1, BF+PF ≤ 1 and BF+HF+PF+IF ≤ 1.
-    
+    Since 1−BF > 0 we need 1−K ≥ 0 or equivalently BF+HF×h+PF×p+IF×h×p ≤ 1.
     We also need 1-K ≤ 1-BF, or equivalently K ≥ BF which reduces to
-    HF*h + PF*p + IF*h*p ≥ 0. Similar to the above, this gives us the requirement
-    that HF ≥ 0, PF ≥ 0 and HF+PF+IF ≥ 0.
+    HF×h + PF×p + IF×h×p ≥ 0.
+    
+    Initially h is 1 (when holeIndex=0), and since holeIndex increases towards
+    infinity with net age, h will tend to 0 (assuming the scaling factor is
+    positive). p will tend towards 0 as the insecticide content tends towards
+    0, however it's initial value p₀ depends on the initial content and the
+    scaling factor. We know 0 ≤ p ≤ p₀ ≤ 1 but not the exact value of p₀ since
+    insecticideContent is sampled from a normal distribution per net; in theory
+    though the maximum sample is unlimited and thus p₀ can take any value less
+    than 1.
+    
+    Substituting the initial values for h and p we require:
+    BF + HF + (PF+IF)×p₀ ≤ 1, and HF + (PF+IF)×p₀ ≥ 0.
+    Since p₀ may be as low as 0, we require HF ≥ 0 and BF + HF ≤ 1. The extreme
+    when both h and p tend to zero doesn't tell us much, but it is also possible
+    for h and p to take values in between. Exactly what limit would be needed
+    in all these cases isn't obvious, but certainly requiring PF ≥ 0 and
+    BF + PF×p₀ ≤ 1 (which assumes h→0 while p=p₀) is sufficient.
+    
+    Since we want limits without p₀, observe that the initial insecticide
+    content is sampled from a normal distribution and thus has no maximum
+    bound, implying that p₀ can take any value less than 1, so substituting p₀
+    with the value 1 is the only way to guarantee that (1−K)/(1−BF) ∈ [0,1].
     */
     if( !( BF+HF <= 1.0 && BF+PF <= 1.0 && BF+HF+PF+IF <= 1.0 &&
         HF >= 0.0 && PF >= 0.0 && HF+PF+IF >= 0.0 ) ){
