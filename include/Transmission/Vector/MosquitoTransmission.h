b928983a32af197beea60d83739f344832c04ac6
diggory.hardy <diggory.hardy@aa1ca794-fd11-11dd-ac65-f74f249cfaac>
2011-08-18 16:16:18 +0000

    New files to go with last commit.
    git-svn-id: https://openmalaria.googlecode.com/svn/branches/vec-lifecycle@2103 aa1ca794-fd11-11dd-ac65-f74f249cfaac

---
+++ include/Transmission/Vector/MosquitoTransmission.h
@@ -0,0 +1,227 @@
+/* This file is part of OpenMalaria.
+ *
+ * Copyright (C) 2005-2011 Swiss Tropical Institute and Liverpool School Of Tropical Medicine
+ *
+ * OpenMalaria is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef Hmod_OM_Transmission_Vector_MosquitoTransmission
+#define Hmod_OM_Transmission_Vector_MosquitoTransmission
+
+#include "Global.h"
+#include "Monitoring/Survey.h"
+#include "Transmission/Vector/MosquitoLifeCycle.h"
+#include "schema/entomology.h"
+
+namespace OM {
+namespace Transmission {
+namespace Vector {
+
+enum VecStat { PA, PDF, PDIF, NV, OV, SV };
+
+/** Encapsulates the central part of the Chitnis et al transmission model:
+ * vector transmission of malaria. The parts associated with mosquito
+ * life-cycle are encapsulated in lifeCycle (MosquitoLifeCycle class).
+ * 
+ * This is only part of the model; the VectorModel class is largely just a
+ * wrapper to support multiple mosquito species, and the Vector::SpeciesModel
+ * class adds parameter initialisation and intervention support to this class
+ * as well as translating between the (1- or 5-day) time-steps used by the
+ * simulator and the 1-day time-step used by this model.
+ */
+class MosquitoTransmission {
+public:
+    /** Initialise parameters and variables.
+     * 
+     * This is only a fraction of parameter initialisation; see also
+     * SpeciesModel::initialise. */
+    void initialise ( const scnXml::AnophelesParams& anoph );
+    
+    /// Checkpointing
+    //Note: below comments about what does and doesn't need checkpointing are ignored here.
+    template<class S>
+    void operator& (S& stream) {
+        mosqRestDuration & stream;
+        EIPDuration & stream;
+        N_v_length & stream;
+        lcParams & stream;
+        P_A & stream;
+        P_df & stream;
+        P_dif & stream;
+        N_v & stream;
+        O_v & stream;
+        S_v & stream;
+        lifeCycle & stream;
+        fArray & stream;
+        ftauArray & stream;
+        timestep_N_v0 & stream;
+    }
+    
+    /** (Re)-initialise some state variables. */
+    void initState ( double tsP_A, double tsP_df );
+    
+    /** Update by one day (may be called multiple times for 1 time-step update).
+     * 
+     * @param d The day whose state we are calculating.
+     * @param tsP_A P_A for this time-step
+     * @param tsP_df P_df for this time-step
+     * @param tsP_dif P_dif for this time-step
+     * @returns S_v
+     */
+    double update( size_t d, double tsP_A, double tsP_df, double tsP_dif );
+    
+    ///@brief Functions called to deploy interventions
+    //@{
+    //void intervLarviciding (/*const scnXml::LarvicidingAnopheles&*/);
+
+    void uninfectVectors();
+    //@}
+
+    inline int getEIPDuration() const {
+        return EIPDuration;
+    }
+    
+    ///@brief Functions used in reporting
+    //@{
+    /// Reset per-time-step statistics before running time-step updates
+    inline void resetTSStats() {
+        timestep_N_v0 = 0.0;
+    }
+    /// Get emergence during last time-step
+    inline double getLastN_v0 () const{
+        return timestep_N_v0;
+    }
+    /// Get P_A/P_df/P_dif/N_v/O_v/S_v during last time-step
+    /// @param vs PA, PDF, PDIF, NV, OV or SV
+    double getLastVecStat ( VecStat vs ) const;
+    
+    inline double getResAvailability() const{
+        return lcParams.getResAvailability();
+    }
+    inline double getResRequirements() const{
+        return lifeCycle.getResRequirements( lcParams );
+    }
+
+    /// Write some per-species summary information.
+    void summarize (const string speciesName, Monitoring::Survey& survey) const;
+    //@}
+    
+private:
+    // -----  parameters (constant after initialisation)  -----
+    
+    /** @brief Duration parameters for mosquito/parasite life-cycle
+     * 
+     * Currently these are all constant. In theory they could be made to vary
+     * seasonally, based on a fixed periodic cycle, though some code and
+     * possibly model changes would be needed to accomodate this.
+     * 
+     * All have units of days.
+     *
+     * Set in initialise function from XML data; no need to checkpoint. */
+    //@{
+    /** Duration of feeding cycle (equals duration of resting period) for
+     * mosquito (τ).
+     * Units: days. */
+    int mosqRestDuration;
+
+    /** Duration of the extrinsic incubation period (sporozoite development time)
+    * (θ_s).
+    * Units: Days.
+    *
+    * Doesn't need checkpointing. */
+    int EIPDuration;
+    
+    /** N_v_length-1 is the number of previous days for which some parameters are
+     * stored: P_A, P_df, P_dif, N_v, O_v and S_v. This is longer than some of
+     * the arrays need to be, but simplifies code with no real impact.
+     *
+     * Should equal EIPDuration + mosqRestDuration to allow values up to
+     * θ_s + τ - 1 days back, plus current day.
+     *
+     * Set by initialise; no need to checkpoint. */
+    int N_v_length;
+    //@}
+
+    /// If less than this many mosquitoes remain infected, transmission is interrupted.
+    double minInfectedThreshold;
+    
+    /// Parameters for life-cycle (excluding parasite transmission)
+    MosqLifeCycleParams lcParams;
+    
+    
+    // -----  variable model state  -----
+    
+    /** @brief Variable arrays N_v_length long.
+     *
+     * P_A, P_df, P_dif, N_v, O_v and S_v are set in advancePeriod().
+     *
+     * Values at index ((d-1) mod N_v_length) are used to derive the state of
+     * the population on day d. The state during days (t×(I-1)+1) through (t×I)
+     * where t is TimeStep::simulation and I is TimeStep::interval is what
+     * drives the transmission at time-step t.
+     * 
+     * These arrays should be checkpointed. */
+    //@{
+    /** Probability of a mosquito not finding a host one night. */
+    vector<double> P_A;
+
+    /** P_df and P_dif per-day.
+     *
+     * P_df is the probability of a mosquito finding a host and completing a
+     * feeding cycle without being killed.
+     *
+     * P_dif is the probability of a mosquito finding a host, getting infected,
+     * and successfully completing a feeding cycle.
+     *
+     * HOWEVER, if the initialisation phase is driven by an input EIR and not by
+     * vector calculations, then during the initialisation phase, P_dif contains
+     * the daily kappa values read from XML for validation purposes. */
+    vector<double> P_df, P_dif;
+
+    /** Numbers of host-seeking mosquitos each day
+     * 
+     * N_v is the total number of host-seeking mosquitoes, O_v is those seeking
+     * and infected, and S_v is those seeking and infective (to humans). */
+    vector<double> N_v, O_v, S_v;
+    //@}
+
+    /// Mosquito life-cycle state
+    MosquitoLifeCycle lifeCycle;
+    
+    ///@brief Working memory
+    /** Used for calculations within advancePeriod. Only saved for optimisation.
+     *
+     * Used to calculate recursive functions f and f_τ in NDEMD eq 1.6, 1.7.
+     * Values are recalculated each step; only fArray[0] and
+     * ftauArray[0..mosqRestDuration] are stored across steps for optimisation
+     * (reallocating each time they are needed would be slow).
+     *
+     * Length (fArray): EIPDuration - mosqRestDuration + 1 (θ_s - τ + 1)
+     * Length (ftauArray): EIPDuration (θ_s)
+     *
+     * Don't need to be checkpointed, but some values need to be initialised. */
+    //@{
+    vector<double> fArray;
+    vector<double> ftauArray;
+    //@}
+    
+    /** Variables tracking data to be reported. */
+    double timestep_N_v0;
+};
+
+}
+}
+}
+#endif
---
+++ model/Transmission/Vector/MosquitoTransmission.cpp
@@ -0,0 +1,238 @@
+/* This file is part of OpenMalaria.
+ *
+ * Copyright (C) 2005-2011 Swiss Tropical Institute and Liverpool School Of Tropical Medicine
+ *
+ * OpenMalaria is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+//TODO: trim includes
+#include "Transmission/Vector/MosquitoTransmission.h"
+#include "schema/entomology.h"
+#include "util/vectors.h"
+#include "util/errors.h"
+#include "util/CommandLine.h"
+#include "util/MultidimSolver.h"
+#include <cmath>
+#include <sstream>
+
+namespace OM {
+namespace Transmission {
+namespace Vector {
+using namespace OM::util;
+
+void MosquitoTransmission::initialise ( const scnXml::AnophelesParams& anoph ) {
+    // -----  Set model variables  -----
+    const scnXml::Mosq& mosq = anoph.getMosq();
+    
+    mosqRestDuration = mosq.getMosqRestDuration().getValue();
+    EIPDuration = mosq.getExtrinsicIncubationPeriod().getValue();
+    if (1 > mosqRestDuration || mosqRestDuration > EIPDuration) {
+        throw util::xml_scenario_error ("Code expects EIPDuration >= mosqRestDuration >= 1");
+    }
+    N_v_length = EIPDuration + mosqRestDuration;
+    
+    minInfectedThreshold = mosq.getMinInfectedThreshold();
+    
+    lcParams.initMosqLifeCycle( anoph.getLifeCycle() );
+    lifeCycle.init( lcParams );
+    
+    // -----  allocate memory  -----
+    // Set up fArray and ftauArray. Each step, all elements not set here are
+    // calculated, even if they aren't directly used in the end;
+    // however all calculated values are used in calculating the next value.
+    fArray.resize(EIPDuration-mosqRestDuration+1);
+    fArray[0] = 1.0;
+    ftauArray.resize(EIPDuration);
+    for (int i = 0; i < mosqRestDuration; ++i)
+        ftauArray[i] = 0.0;
+    ftauArray[mosqRestDuration] = 1.0;
+
+    N_v  .resize (N_v_length);
+    O_v  .resize (N_v_length);
+    S_v  .resize (N_v_length);
+    P_A  .resize (N_v_length);
+    P_df .resize (N_v_length);
+    P_dif.resize (N_v_length);
+}
+
+void MosquitoTransmission::initState ( double tsP_A, double tsP_df ){
+    // Initialize per-day variables; S_v, N_v and O_v are only estimated
+    for (int t = 0; t < N_v_length; ++t) {
+        P_A[t] = tsP_A;
+        P_df[t] = tsP_df;
+        P_dif[t] = 0.0;     // humans start off with no infectiousness.. so just wait
+        /* FIXME
+        S_v[t] = forcedS_v[t];  // assume N_v_length ≤ 365
+        N_v[t] = S_v[t] * initNvFromSv;
+        O_v[t] = S_v[t] * initOvFromSv;
+        */
+    }
+}
+
+double MosquitoTransmission::update( size_t d, double tsP_A, double tsP_df, double tsP_dif ){
+    // Warning: with x<0, x%y can be negative (depending on compiler); avoid x<0.
+    // We add N_v_length so that ((dMod - x) >= 0) for (x <= N_v_length).
+    size_t dMod = d + N_v_length;
+    assert (dMod >= (size_t)N_v_length);
+    // Indecies for today, yesterday and mosqRestDuration days back:
+    size_t t    = dMod % N_v_length;
+    size_t t1   = (dMod - 1) % N_v_length;
+    size_t ttau = (dMod - mosqRestDuration) % N_v_length;
+    // Day of year and of 5-year cycles. Note that emergence during day 1
+    // comes from mosqEmergeRate[0], hence subtraction by 1.
+    size_t dYear1 = (d - 1) % TimeStep::DAYS_IN_YEAR;
+    size_t d5Year = d % (TimeStep::DAYS_IN_YEAR * 5);
+    
+    
+    // These only need to be calculated once per timestep, but should be
+    // present in each of the previous N_v_length - 1 positions of arrays.
+    P_A[t] = tsP_A;
+    P_df[t] = tsP_df;
+    P_dif[t] = tsP_dif;
+    
+    
+    // update life-cycle model
+    double newAdults = lifeCycle.updateEmergence( lcParams,
+                                                    P_df[ttau] * N_v[ttau],
+                                                    d, dYear1
+                                                );
+    
+    
+    // num seeking mosquitos is: new adults + those which didn't find a host
+    // yesterday + those who found a host tau days ago and survived cycle:
+    N_v[t] = newAdults
+                + P_A[t1]  * N_v[t1]
+                + P_df[ttau] * N_v[ttau];
+    // similar for O_v, except new mosquitoes are those who were uninfected
+    // tau days ago, started a feeding cycle then, survived and got infected:
+    O_v[t] = P_dif[ttau] * (N_v[ttau] - O_v[ttau])
+                + P_A[t1]  * O_v[t1]
+                + P_df[ttau] * O_v[ttau];
+
+
+    //BEGIN S_v
+    // Set up array with n in 1..θ_s−1 for f_τ(dMod-n) (NDEMD eq. 1.7)
+    size_t fProdEnd = 2*mosqRestDuration;
+    for (size_t n = mosqRestDuration+1; n <= fProdEnd; ++n) {
+        size_t tn = (dMod-n)%N_v_length;
+        ftauArray[n] = ftauArray[n-1] * P_A[tn];
+    }
+    ftauArray[fProdEnd] += P_df[(dMod-fProdEnd)%N_v_length];
+
+    for (int n = fProdEnd+1; n < EIPDuration; ++n) {
+        size_t tn = (dMod-n)%N_v_length;
+        ftauArray[n] =
+            P_df[tn] * ftauArray[n - mosqRestDuration]
+            + P_A[tn] * ftauArray[n-1];
+    }
+
+    double sum = 0.0;
+    size_t ts = dMod - EIPDuration;
+    for (int l = 1; l < mosqRestDuration; ++l) {
+        size_t tsl = (ts - l) % N_v_length;       // index dMod - theta_s - l
+        sum += P_dif[tsl] * P_df[ttau] * (N_v[tsl] - O_v[tsl]) * ftauArray[EIPDuration+l-mosqRestDuration];
+    }
+
+
+    // Set up array with n in 1..θ_s−τ for f(dMod-n) (NDEMD eq. 1.6)
+    for (int n = 1; n <= mosqRestDuration; ++n) {
+        size_t tn = (dMod-n)%N_v_length;
+        fArray[n] = fArray[n-1] * P_A[tn];
+    }
+    fArray[mosqRestDuration] += P_df[ttau];
+
+    fProdEnd = EIPDuration-mosqRestDuration;
+    for (size_t n = mosqRestDuration+1; n <= fProdEnd; ++n) {
+        size_t tn = (dMod-n)%N_v_length;
+        fArray[n] =
+            P_df[tn] * fArray[n - mosqRestDuration]
+            + P_A[tn] * fArray[n-1];
+    }
+
+
+    ts = ts % N_v_length;       // index dMod - theta_s
+    S_v[t] = P_dif[ts] * fArray[EIPDuration-mosqRestDuration] * (N_v[ts] - O_v[ts])
+                + sum
+                + P_A[t1]*S_v[t1]
+                + P_df[ttau]*S_v[ttau];
+
+
+    // We cut-off transmission when no more than X mosquitos are infected to
+    // allow true elimination in simulations. Unfortunately, it may cause problems with
+    // trying to simulate extremely low transmission, such as an R_0 case.
+    if ( S_v[t] <= minInfectedThreshold ) { // infectious mosquito cut-off
+        S_v[t] = 0.0;
+        /* Note: could report; these reports often occur too frequently, however
+        if( S_v[t] != 0.0 ){        // potentially reduce reporting
+            cerr << TimeStep::simulation <<":\t S_v cut-off"<<endl;
+        } */
+    }
+    //END S_v
+
+
+    //FIXME quinquennialS_v[d5Year] = S_v[t];
+    
+    timestep_N_v0 += newAdults;
+
+    return S_v[t];
+}
+
+
+// -----  Summary and intervention functions  -----
+
+/*FIXME
+void MosquitoTransmission::intervLarviciding (const scnXml::LarvicidingAnopheles& elt) {
+    cerr << "This larviciding implementation isn't valid (according to NC)." << endl;
+    larvicidingIneffectiveness = 1 - elt.getEffectiveness();
+    larvicidingEndStep = TimeStep::simulation + TimeStep::fromDays(elt.getDuration());
+}
+*/
+void MosquitoTransmission::uninfectVectors() {
+    O_v.assign( O_v.size(), 0.0 );
+    S_v.assign( S_v.size(), 0.0 );
+    P_dif.assign( P_dif.size(), 0.0 );
+}
+
+double MosquitoTransmission::getLastVecStat ( VecStat vs ) const{
+    //Note: implementation isn't performance optimal but rather intended to
+    //keep code size low and have no overhead if not used.
+    const vector<double> *array;
+    switch( vs ){
+        case PA: array = &P_A; break;
+        case PDF: array = &P_df; break;
+        case PDIF: array = &P_dif; break;
+        case NV: array = &N_v; break;
+        case OV: array = &O_v; break;
+        case SV: array = &S_v; break;
+        default: assert( false );
+    }
+    double val = 0.0;
+    int firstDay = TimeStep::simulation.inDays() - TimeStep::interval + 1;
+    for (size_t i = 0; i < (size_t)TimeStep::interval; ++i) {
+        size_t t = (i + firstDay) % N_v_length;
+        val += (*array)[t];
+    }
+    return val;
+}
+void MosquitoTransmission::summarize (const string speciesName, Monitoring::Survey& survey) const{
+    survey.set_Vector_Nv0 (speciesName, timestep_N_v0/TimeStep::interval);
+    survey.set_Vector_Nv (speciesName, getLastVecStat(NV)/TimeStep::interval);
+    survey.set_Vector_Ov (speciesName, getLastVecStat(OV)/TimeStep::interval);
+    survey.set_Vector_Sv (speciesName, getLastVecStat(SV)/TimeStep::interval);
+}
+
+}
+}
+}
