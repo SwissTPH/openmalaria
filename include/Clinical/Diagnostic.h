d425bd673c421e39489f07d2d185a435973736b0
Diggory Hardy <diggory.hardy@gmail.com>
2013-04-08 19:43:21 +0200

    Update/unify copyright headers

--- include/Clinical/Diagnostic.h
+++ include/Clinical/Diagnostic.h
@@ -1,6 +1,7 @@
 /* This file is part of OpenMalaria.
  * 
- * Copyright (C) 2005-2011 Swiss Tropical Institute and Liverpool School Of Tropical Medicine
+ * Copyright (C) 2005-2013 Swiss Tropical and Public Health Institute 
+ * Copyright (C) 2005-2013 Liverpool School Of Tropical Medicine
  * 
  * OpenMalaria is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
