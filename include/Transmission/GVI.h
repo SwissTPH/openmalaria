ac9e3355c922b61052428a9ce1c3c7ccbee9a867
Diggory Hardy <diggory.hardy@gmail.com>
2013-08-05 22:12:49 +0200

    Add some very pertinent files to the previous commit.

---
+++ include/Transmission/GVI.h
@@ -0,0 +1,129 @@
+/* This file is part of OpenMalaria.
+ * 
+ * Copyright (C) 2005-2013 Swiss Tropical and Public Health Institute 
+ * Copyright (C) 2005-2013 Liverpool School Of Tropical Medicine
+ * 
+ * OpenMalaria is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef TRANSMISSION_GVI
+#define TRANSMISSION_GVI
+
+#include "util/DecayFunction.h"
+#include "util/sampler.h"
+#include "schema/interventions.h"
+#include <boost/shared_ptr.hpp>
+
+namespace OM { namespace Transmission {
+    using util::DecayFunction;
+    using util::DecayFuncHet;
+    using util::NormalSampler;
+    using util::LognormalSampler;
+    using boost::shared_ptr;
+    // forward declare:
+    class TransmissionModel;
+
+/** Constant parameters for generic vector intervention model. */
+class GVIParams {
+public:
+    GVIParams() :  maxInsecticide(numeric_limits< double >::signaling_NaN()) {}
+    /** Set parameters */
+    void init( const scnXml::GVIDescription& elt);
+    
+private:
+    NormalSampler initialInsecticide;
+    double maxInsecticide;		// maximum initial insecticide
+    shared_ptr<DecayFunction> decay;
+    
+    friend class GVI;
+    friend class GVIAnophelesParams;
+};
+
+/** Per mosquito-species parameters for generic vector intervention model. */
+class GVIAnophelesParams {
+public:
+    GVIAnophelesParams( const GVIParams* b ) :
+        base( b ),
+        proportionProtected( numeric_limits<double>::signaling_NaN() ),
+        proportionUnprotected( numeric_limits<double>::signaling_NaN() ),
+        _relativeAttractiveness( numeric_limits<double>::signaling_NaN() ),
+        _preprandialKillingEffect( numeric_limits<double>::signaling_NaN() ),
+        _postprandialKillingEffect( numeric_limits<double>::signaling_NaN() )
+    {}
+    void init(const GVIParams& params,
+              const scnXml::GVIDescription::AnophelesParamsType& elt);
+    
+    /// Return x*proportionProtected + proportionUnprotected
+    inline double byProtection(double x) const{
+        return x*proportionProtected + proportionUnprotected;
+    }
+    
+private:
+    const GVIParams* base;
+    double proportionProtected;
+    double proportionUnprotected;
+    double _relativeAttractiveness;
+    double _preprandialKillingEffect;
+    double _postprandialKillingEffect;
+    
+    friend class GVI;
+};
+
+/** Low-level (generic) vector intervention model. Has three effects:
+ * deterrency, pre-prandial killing and post-prandial killing.
+ */
+class GVI {
+public:
+    GVI (const TransmissionModel& tm);
+    
+    /// Checkpointing
+    template<class S>
+    void operator& (S& stream) {
+        deployTime & stream;
+        initialInsecticide & stream;
+        decayHet & stream;
+    }
+    
+    void deploy(const GVIParams& params);
+    inline TimeStep timeOfDeployment()const{
+        return deployTime;
+    }
+    /** This is the survival factor of the effect. */
+    inline double getEffectSurvival(const GVIParams& params)const{
+        return params.decay->eval (TimeStep::simulation - deployTime,
+                                              decayHet);
+    }
+    
+    /// Get deterrency. See ComponentParams::effect for a more detailed description.
+    double relativeAttractiveness(const GVIAnophelesParams& params) const;
+    /// Get killing effect on mosquitoes before they've eaten.
+    /// See ComponentParams::effect for a more detailed description.
+    double preprandialSurvivalFactor(const GVIAnophelesParams& params) const;
+    /// Get killing effect on mosquitoes after they've eaten.
+    /// See ComponentParams::effect for a more detailed description.
+    double postprandialSurvivalFactor(const GVIAnophelesParams& params) const;
+    
+private:
+    // these parameters express the current state of the intervention:
+    TimeStep deployTime;	// time of deployment or TimeStep::never
+    double initialInsecticide;	// units: mg/m²
+    
+    // this parameters are sampled from log-normal per intervention, but thereafter constant:
+    DecayFuncHet decayHet;
+};
+
+} }
+
+#endif
---
+++ model/Transmission/GVI.cpp
@@ -0,0 +1,94 @@
+/* This file is part of OpenMalaria.
+ * 
+ * Copyright (C) 2005-2013 Swiss Tropical and Public Health Institute 
+ * Copyright (C) 2005-2013 Liverpool School Of Tropical Medicine
+ * 
+ * OpenMalaria is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "Transmission/GVI.h"
+//TODO: we shouldn't have a dependency on the vector/transmission model class
+//here; currently it's a work-around for GVI parameters not always being present.
+#include "Transmission/VectorModel.h"
+#include "util/random.h"
+#include "util/errors.h"
+#include "R_nmath/qnorm.h"
+#include <cmath>
+
+namespace OM { namespace Transmission {
+    using util::random::poisson;
+
+void GVIParams::init( const scnXml::GVIDescription& elt) {
+    decay = DecayFunction::makeObject( elt.getDecay(), "interventions.human.vector.decay" );
+}
+
+void GVIAnophelesParams::init(
+    const GVIParams& params,
+    const scnXml::GVIDescription::AnophelesParamsType& elt)
+{
+    if( _relativeAttractiveness == _relativeAttractiveness ){
+        throw util::unimplemented_exception( "multiple GVI interventions" );
+    }
+    _relativeAttractiveness = elt.getDeterrency().getValue();
+    _preprandialKillingEffect = elt.getPreprandialKillingEffect().getValue();
+    _postprandialKillingEffect = elt.getPostprandialKillingEffect().getValue();
+    // Simpler version of ITN usage/action:
+    double propActive = elt.getPropActive();
+    assert( propActive >= 0.0 && propActive <= 1.0 );
+    proportionProtected = propActive;
+    proportionUnprotected = 1.0 - proportionProtected;
+}
+
+
+// ———  per-human data  ———
+GVI::GVI (const TransmissionModel& tm) :
+    initialInsecticide( 0.0 )   // start with no insecticide (for monitoring)
+{
+    //TODO: we shouldn't really have vector intervention data (this class) if there's no vector
+    // model, should we? Allocate dynamically or based on model?
+    const VectorModel* vt = dynamic_cast<const VectorModel*>(&tm);
+    if( vt != 0 ){
+        const GVIParams& params = vt->getGVIParams();
+        if( params.decay.get() == 0 )
+            return;     // no intervention
+        // Varience factor of decay is sampled once per human: human is assumed
+        // to account for most variance.
+        decayHet = params.decay->hetSample();
+    }
+}
+
+void GVI::deploy(const GVIParams& params) {
+    deployTime = TimeStep::simulation;
+}
+
+double GVI::relativeAttractiveness(const GVIAnophelesParams& params) const{
+    double effect = (1.0 - params._relativeAttractiveness *
+            getEffectSurvival(*params.base));
+    return params.byProtection( effect );
+}
+
+double GVI::preprandialSurvivalFactor(const GVIAnophelesParams& params) const{
+    double effect = (1.0 - params._preprandialKillingEffect *
+            getEffectSurvival(*params.base));
+    return params.byProtection( effect );
+}
+
+double GVI::postprandialSurvivalFactor(const GVIAnophelesParams& params) const{
+    double effect = (1.0 - params._postprandialKillingEffect *
+            getEffectSurvival(*params.base));
+    return params.byProtection( effect );
+}
+
+} }
