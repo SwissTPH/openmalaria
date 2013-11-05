f7375625e0e44bfa106bf8cd3222e231bbf6bf64
Diggory Hardy <diggory.hardy@gmail.com>
2013-11-05 13:24:11 +0100

    Reorganise some intervention code

---
+++ include/interventions/HumanInterventionEffects.hpp
@@ -0,0 +1,221 @@
+/* This file is part of OpenMalaria.
+ * 
+ * Copyright (C) 2005-2013 Swiss Tropical and Public Health Institute 
+ * Copyright (C) 2005-2013 Liverpool School Of Tropical Medicine
+ * 
+ * OpenMalaria is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+// Note: this file is included by exactly one source (Interventions.cpp)
+// and contains definitions as well as declarations.
+
+// The includes here are more for documentation than required.
+#include "interventions/Interventions.h"
+#include "Monitoring/Surveys.h"
+#include "util/ModelOptions.h"
+#include "Clinical/ESCaseManagement.h"
+#include "Clinical/ImmediateOutcomes.h"
+#include "Clinical/Diagnostic.h"
+#include "Clinical/CaseManagementCommon.h"
+#include "Host/Human.h"
+#include "Transmission/TransmissionModel.h"
+#include "WithinHost/DescriptiveIPTWithinHost.h"
+#include <schema/healthSystem.h>
+#include <schema/interventions.h>
+
+namespace OM { namespace interventions {
+    using Host::Human;
+
+class MDAEffect : public HumanInterventionEffect {
+public:
+    MDAEffect( size_t index, const scnXml::MDA& mda ) : HumanInterventionEffect(index) {
+        if( !mda.getDiagnostic().present() ){
+            // Note: allow no description for now to avoid XML changes.
+            //throw util::xml_scenario_error( "error: interventions.MDA.diagnostic element required for MDA with 5-day timestep" );
+            scnXml::HSDiagnostic diagnostic;
+            scnXml::Deterministic det(0.0);
+            diagnostic.setDeterministic(det);
+            this->diagnostic.init(diagnostic);
+        }else{
+            diagnostic.init( mda.getDiagnostic().get() );
+        }
+        const scnXml::DrugWithCompliance& drug = mda.getDrugEffect();
+        double pCompliance = drug.getCompliance().getPCompliance();
+        double nonComplierMult = drug.getCompliance().getNonCompliersMultiplier();
+        double mult = pCompliance + (1.0 - pCompliance) * nonComplierMult;
+        const scnXml::CompliersEffective::TimestepSequence& seq =
+                drug.getCompliersEffective().getTimestep();
+        size_t len = seq.size();
+        pClearanceByTime.resize(len);
+        for( size_t i = 0; i < len; ++i ){
+            double pCompliers = seq[i].getPClearance();
+            pClearanceByTime[i] = mult * pCompliers;
+        }
+        if( len < 1 ){
+            throw util::xml_scenario_error(
+                "interventions.human.effect.MDA.drugEffect: require at "
+                "least one timestep element" );
+        }
+        if( len > 1 ){
+            if( !util::ModelOptions::option( util::PROPHYLACTIC_DRUG_ACTION_MODEL ) )
+                throw util::xml_scenario_error(
+                    "MDA with prophylactic effect (i.e. with more than one"
+                    " timestep element in drugEffect element) requires the"
+                    " PROPHYLACTIC_DRUG_ACTION_MODEL" );
+        }   
+    }
+    
+    void deploy( Human& human, Deployment::Method method ) const{
+        //TODO: shouldn't really use the same reports for mass and continuous deployment, right?
+        
+        Monitoring::Surveys.getSurvey(human.isInAnyCohort())
+                .reportMassScreening(human.getMonitoringAgeGroup(), 1);
+        if( !diagnostic.isPositive( human.withinHostModel->getTotalDensity() ) ){
+            return;
+        }
+        Monitoring::Surveys.getSurvey(human.isInAnyCohort())
+                .reportMDA(human.getMonitoringAgeGroup(), 1);
+        double pClearance = pClearanceByTime[0];
+        if( pClearance >= 1.0 || util::random::bernoulli( pClearance ) ){
+            human.withinHostModel->clearInfections(human.getClinicalModel().latestIsSevere());
+        }
+        if( pClearanceByTime.size() > 1 )
+            human.withinHostModel->addProphylacticEffects( pClearanceByTime );
+    }
+    
+    virtual Effect::Type effectType() const{ return Effect::MDA; }
+    
+private:
+    Clinical::Diagnostic diagnostic;
+    vector<double> pClearanceByTime;
+};
+
+class MDA1DEffect : public HumanInterventionEffect {
+public:
+    MDA1DEffect( size_t index, const scnXml::MDA1D& description ) : HumanInterventionEffect(index) {
+        Clinical::ESCaseManagement::initMDA( description );
+    }
+    
+    void deploy( Human& human, Deployment::Method method ) const{
+        human.massDrugAdministration();
+    }
+    
+    virtual Effect::Type effectType() const{ return Effect::MDA_TS1D; }
+    
+private:
+};
+
+class VaccineEffect : public HumanInterventionEffect {
+public:
+    VaccineEffect( size_t index, const scnXml::VaccineDescription& seq, Host::Vaccine::Types type ) :
+            HumanInterventionEffect(index), type(type)
+    {
+        Host::Vaccine::types[type].initVaccine( seq, type );
+    }
+    
+    void deploy( Human& human, Deployment::Method method )const{
+        human.deployVaccine( method, type );
+    }
+    
+    virtual Effect::Type effectType() const{
+        if( type == Host::Vaccine::PEV ) return Effect::PEV;
+        else if( type == Host::Vaccine::BSV ) return Effect::BSV;
+        else if( type == Host::Vaccine::TBV ) return Effect::TBV;
+        else throw SWITCH_DEFAULT_EXCEPTION;
+    }
+    
+    inline Host::Vaccine::Types getVaccineType()const{ return type; }
+    
+private:
+    Host::Vaccine::Types type;
+};
+
+class IPTEffect : public HumanInterventionEffect {
+public:
+    IPTEffect( size_t index, const scnXml::IPTDescription& elt ) : HumanInterventionEffect(index){
+        // Check compatibilities with MDA model, in particular use of clearInfections(bool).
+        // Is it needed now MDA allows prophylactic effects and continuous deployment, anyway?
+        throw util::unimplemented_exception( "IPT model (disabled pending review)" );
+        WithinHost::DescriptiveIPTWithinHost::init( elt );
+    }
+    
+    void deploy( Human& human, Deployment::Method method )const{
+        human.deployIPT( method );
+    }
+    
+    virtual Effect::Type effectType() const{ return Effect::IPT; }
+};
+
+class ITNEffect : public HumanInterventionEffect {
+public:
+    ITNEffect( size_t index, const scnXml::ITNDescription& elt,
+               Transmission::TransmissionModel& transmissionModel ) : HumanInterventionEffect(index),
+               transmission( transmissionModel )
+    {
+        transmissionModel.setITNDescription( elt );
+    }
+    
+    void deploy( Human& human, Deployment::Method method )const{
+        human.deployITN( method, transmission );
+    }
+    
+    virtual Effect::Type effectType() const{ return Effect::ITN; }
+    
+private:
+    Transmission::TransmissionModel& transmission;      //TODO: storing this is not a nice solution; do we need to pass?
+};
+
+class IRSEffect : public HumanInterventionEffect {
+public:
+    IRSEffect( size_t index, const scnXml::IRSDescription& elt,
+               Transmission::TransmissionModel& transmissionModel ) : HumanInterventionEffect(index),
+               transmission( transmissionModel )
+    {
+        transmissionModel.setIRSDescription( elt );
+    }
+    
+    void deploy( Human& human, Deployment::Method method )const{
+        human.deployIRS( method, transmission );
+    }
+    
+    virtual Effect::Type effectType() const{ return Effect::IRS; }
+    
+private:
+    Transmission::TransmissionModel& transmission;      //TODO: storing this is not a nice solution; do we need to pass?
+};
+
+class CohortSelectionEffect : public HumanInterventionEffect {
+public:
+    CohortSelectionEffect( size_t index ) : HumanInterventionEffect(index) {}
+    
+    void deploy( Human& human, Deployment::Method method )const{
+        human.addToCohort( getIndex() );
+    }
+    
+    virtual Effect::Type effectType() const{ return Effect::COHORT; }
+};
+
+class ClearImmunityEffect : public HumanInterventionEffect {
+public:
+    ClearImmunityEffect( size_t index ) : HumanInterventionEffect(index) {}
+    
+    void deploy( Human& human, Deployment::Method method )const{
+        human.clearImmunity();
+    }
+    
+    virtual Effect::Type effectType() const{ return Effect::CLEAR_IMMUNITY; }
+};
+
+} }
---
+++ include/interventions/TimedDeployments.hpp
@@ -0,0 +1,254 @@
+/* This file is part of OpenMalaria.
+ * 
+ * Copyright (C) 2005-2013 Swiss Tropical and Public Health Institute 
+ * Copyright (C) 2005-2013 Liverpool School Of Tropical Medicine
+ * 
+ * OpenMalaria is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+// Note: this file is included by exactly one source (Interventions.cpp)
+// and contains definitions as well as declarations.
+
+// The includes here are more for documentation than required.
+#include "util/errors.h"
+#include "util/TimeStep.h"
+#include "Monitoring/Surveys.h"
+#include <schema/interventions.h>
+#include "Clinical/CaseManagementCommon.h"
+#include "Population.h"
+#include "Transmission/TransmissionModel.h"
+
+namespace OM { namespace interventions {
+
+// ———  TimedDeployment and derivatives  ———
+
+/** Interface for timed deployment of an intervention. */
+class TimedDeployment {
+public:
+    /// Create, passing time of deployment
+    explicit TimedDeployment(TimeStep deploymentTime) :
+            time( deploymentTime )
+    {
+        if( deploymentTime < TimeStep(0) ){
+            throw util::xml_scenario_error("timed intervention deployment: may not be negative");
+        }else if( deploymentTime >= Monitoring::Surveys.getFinalTimestep() ){
+            cerr << "Warning: timed intervention deployment at time "<<deploymentTime.asInt();
+            cerr << " happens after last survey" << endl;
+        }
+    }
+    virtual ~TimedDeployment() {}
+    
+    bool operator< (const TimedDeployment& that) const{
+        return this->time < that.time;
+    }
+    
+    virtual void deploy (OM::Population&) =0;
+    
+    // Read access required in this file; don't really need protection:
+    TimeStep time;
+};
+
+class DummyTimedDeployment : public TimedDeployment {
+public:
+    DummyTimedDeployment() :
+        TimedDeployment( TimeStep(0) )
+    {
+        // TimedDeployment's ctor checks that the deployment time-step is
+        // within the intervention period. We want this time to be after the
+        // last time-step, so set the time here after TimedDeployment's ctor
+        // check has been done (hacky).
+        time = TimeStep::future;
+    }
+    virtual void deploy (OM::Population&) {}
+};
+
+class TimedChangeHSDeployment : public TimedDeployment {
+public:
+    TimedChangeHSDeployment( const scnXml::ChangeHS::TimedDeploymentType& hs ) :
+        TimedDeployment( TimeStep( hs.getTime() ) ),
+        newHS( hs._clone() )
+    {}
+    virtual void deploy (OM::Population& population) {
+        Clinical::CaseManagementCommon::changeHealthSystem( *newHS );
+        delete newHS;
+        newHS = 0;
+    }
+    
+private:
+    scnXml::HealthSystem *newHS;
+};
+
+class TimedChangeEIRDeployment : public TimedDeployment {
+public:
+    TimedChangeEIRDeployment( const scnXml::ChangeEIR::TimedDeploymentType& nv ) :
+        TimedDeployment( TimeStep( nv.getTime() ) ),
+        newEIR( nv._clone() )
+    {}
+    virtual void deploy (OM::Population& population) {
+        population.transmissionModel().changeEIRIntervention( *newEIR );
+        delete newEIR;
+        newEIR = 0;
+    }
+    
+private:
+    scnXml::NonVector *newEIR;
+};
+
+class TimedUninfectVectorsDeployment : public TimedDeployment {
+public:
+    TimedUninfectVectorsDeployment( TimeStep deployTime ) :
+        TimedDeployment( deployTime )
+    {}
+    virtual void deploy (OM::Population& population) {
+        population.transmissionModel().uninfectVectors();
+    }
+};
+
+class TimedR_0Deployment : public TimedDeployment {
+public:
+    TimedR_0Deployment( TimeStep deployTime ) :
+        TimedDeployment( deployTime )
+    {}
+    virtual void deploy (OM::Population& population) {
+        int i = (int)std::floor (util::random::uniform_01() * population.size());        // pick a human
+        Population::Iter it = population.begin();
+        while (i > 0){  // find human (can't use population[i])
+            ++it;
+            --i;
+        }
+        assert( i == 0 );
+        assert( it != population.end() );
+        it->R_0Vaccines();
+        it->addInfection();
+    }
+};
+
+/// Timed deployment of human-specific interventions
+class TimedHumanDeployment : public TimedDeployment {
+public:
+    /** 
+     * @param mass XML element specifying the age range and compliance
+     * (proportion of eligible individuals who receive the intervention).
+     * @param intervention The HumanIntervention to deploy.
+     * @param cohort The cohort to which to deploy, or max value */
+    TimedHumanDeployment( const scnXml::Mass& mass,
+                           const HumanIntervention* intervention,
+                           size_t cohort ) :
+        TimedDeployment( TimeStep( mass.getTime() ) ),
+        minAge( TimeStep::fromYears( mass.getMinAge() ) ),
+        maxAge( TimeStep::fromYears( mass.getMaxAge() ) ),
+        cohort( cohort ),
+        coverage( mass.getCoverage() ),
+        intervention( intervention )
+    {
+        if( !(coverage >= 0.0 && coverage <= 1.0) ){
+            throw util::xml_scenario_error("timed intervention coverage must be in range [0,1]");
+        }
+        if( minAge < TimeStep(0) || maxAge < minAge ){
+            throw util::xml_scenario_error("timed intervention must have 0 <= minAge <= maxAge");
+        }
+    }
+    
+    virtual void deploy (OM::Population& population) {
+        for (Population::Iter iter = population.begin(); iter != population.end(); ++iter) {
+            TimeStep age = TimeStep::simulation - iter->getDateOfBirth();
+            if( age >= minAge && age < maxAge ){
+                if( iter->isInCohort( cohort) ){
+                    if( util::random::uniform_01() < coverage ){
+                        intervention->deploy( *iter, Deployment::TIMED );
+                    }
+                }
+            }
+        }
+    }
+    
+protected:
+    // restrictions on deployment
+    TimeStep minAge;
+    TimeStep maxAge;
+    size_t cohort;
+    double coverage;    // proportion coverage within group meeting above restrictions
+    const HumanIntervention *intervention;
+};
+
+/// Timed deployment of human-specific interventions in cumulative mode
+class TimedCumulativeHumanDeployment : public TimedHumanDeployment {
+public:
+    /** 
+     * @param mass XML element specifying the age range and compliance
+     * (proportion of eligible individuals who receive the intervention).
+     * @param intervention The HumanIntervention to deploy.
+     * @param effect_index Index of effect to test coverage for
+     * @param maxAge Maximum time-span to consider a deployed effect still to be effective */
+    TimedCumulativeHumanDeployment( const scnXml::Mass& mass,
+                           const HumanIntervention* intervention,
+                           size_t cohort,
+                           size_t effect_index, TimeStep maxAge ) :
+        TimedHumanDeployment( mass, intervention, cohort ),
+        cumCovInd( effect_index ), maxInterventionAge( maxAge )
+    {
+    }
+    
+    virtual void deploy (OM::Population& population) {
+        // Cumulative case: bring target group's coverage up to target coverage
+        vector<Host::Human*> unprotected;
+        size_t total = 0;       // number of humans within age bound and optionally cohort
+        for (Population::Iter iter = population.begin(); iter != population.end(); ++iter) {
+            TimeStep age = TimeStep::simulation - iter->getDateOfBirth();
+            if( age >= minAge && age < maxAge ){
+                if( iter->isInCohort( cohort ) ){
+                    total+=1;
+                    if( iter->needsRedeployment(cumCovInd, maxInterventionAge) )
+                        unprotected.push_back( &*iter );
+                }
+            }
+        }
+        
+        double propProtected = static_cast<double>( total - unprotected.size() ) / static_cast<double>( total );
+        if( propProtected < coverage ){
+            // Proportion propProtected are already covered, so need to
+            // additionally cover the proportion (coverage - propProtected),
+            // selected from the list unprotected.
+            double additionalCoverage = (coverage - propProtected) / (1.0 - propProtected);
+            for (vector<Host::Human*>::iterator iter = unprotected.begin();
+                 iter != unprotected.end(); ++iter)
+            {
+                if( util::random::uniform_01() < additionalCoverage ){
+                    intervention->deploy( **iter, Deployment::TIMED );
+                }
+            }
+        }
+    }
+    
+protected:
+    size_t cumCovInd;
+    // max age at which an intervention is considered not to need replacement
+    TimeStep maxInterventionAge;
+};
+
+class TimedVectorDeployment : public TimedDeployment {
+public:
+    TimedVectorDeployment( TimeStep deployTime, size_t instance ) :
+        TimedDeployment( deployTime ),
+        inst(instance)
+    {}
+    virtual void deploy (OM::Population& population) {
+      population.transmissionModel().deployVectorPopInterv(inst);
+    }
+private:
+    size_t inst;
+};
+
+} }
