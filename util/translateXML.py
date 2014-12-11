c7fa314c5062456ba58bb41e2e1d490a0ae5c031
Diggory Hardy <diggory.hardy@unibas.ch>
2014-12-11 17:14:04 +0100

    Update translateXML.py: ability to update DB and enable Molineaux model

--- util/translateXML.py
+++ util/translateXML.py
@@ -1,12 +1,34 @@
-#!/bin/env python2
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-# Note: ElementTree appears to have bugs in Python 3.3, but works in 2.7
-# Most of this code works in both Python 2.x and 3.x, with a little compatibilty option:
+"""
+This file is part of OpenMalaria.
+
+Copyright (C) 2005-2014 Swiss Tropical Institute
+
+OpenMalaria is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+"""
+"""
+Tool for translating OpenMalaria XML files from one version to the next
+
+Note: ElementTree appears to have bugs in Python 3.3, but works in 2.7
+Most of this code works in both Python 2.x and 3.x, with a little compatibilty option:
+"""
 from __future__ import print_function
 
-# Tool for translating OpenMalaria XML files from one version to the next
-
 import argparse, os, sys
+
 # We should be able to use lxml or ElementTree. The former may have better formatting.
 try:
     from lxml import etree as ET
@@ -14,6 +36,9 @@ try:
 except ImportError:
     import xml.etree.ElementTree as ET
     using_lxml=False
+
+import MySQLdb
+import getpass
 
 LATEST_VERSION=33
 KNOWN_VERSIONS=[32,33]    # all valid translation targets
@@ -50,16 +75,22 @@ def indent(elem, level=0):
             elem.tail = i
 
 class Translator:
-    def __init__(self,f):
-        if using_lxml:
-            self.tree = ET.parse(f)
+    def __init__(self,f=None,text=None,interval=None):
+        if f != None:
+            assert text == None
+            if using_lxml:
+                self.tree = ET.parse(f)
+            else:
+                self.tree = ET.parse(f, ET.XMLParser(target=MyTreeBuilder()))
+            self.root = self.tree.getroot()
         else:
-            self.tree = ET.parse(f, ET.XMLParser(target=MyTreeBuilder()))
-        self.root = self.tree.getroot()
+            assert text != None
+            # TODO: does this preserve comments when not using lxml?
+            self.root = ET.fromstring(text)
         self.schema_ver = int(self.root.get('schemaVersion', 0))
         if self.schema_ver not in KNOWN_VERSIONS:
             err = DocumentError('Unknown document or document version')
-            err << 'Found version '+version
+            err << 'Found version '+str(self.schema_ver)
             err << 'Known versions: '+str(KNOWN_VERSIONS)
             raise err
         self.namespace = 'http://openmalaria.org/schema/scenario_' + str(self.schema_ver)
@@ -68,21 +99,27 @@ class Translator:
             err << 'Expected <'+self.tag_name()+'>'
             err << 'Found <'+self.root.tag+'>'
             raise err
-        self.interval=int(self.root.find('model').find('parameters').get('interval'))
+        self.interval=int(interval if interval != None else self.root.find('model').find('parameters').get('interval'))
         if self.interval not in (1,5):
             raise DocumentError('Unexpected time step: '+str(self.interval))
         self.steps_per_year = 365 / self.interval
     
-    def write_to(self,f):
+    def to_string(self):
         indent(self.root)
-        if using_lxml:
-            self.tree.write(f, encoding='UTF-8', xml_declaration=True, pretty_print=False)
-        else:
-            ET.register_namespace('om',self.namespace)
-            self.tree.write(f, encoding='UTF-8', xml_declaration=True)
+        ET.register_namespace('om',self.namespace)
+        return ET.tostring(self.root, encoding='UTF-8')
     
-    def update_to(self,target_version):
-        while self.schema_ver < target_version:
+    def write_to(self,f):
+        """Write a tree to a file. Incompatible with reading from DB, since that only gives us a root."""
+        indent(self.root)
+        ET.register_namespace('om',self.namespace)
+        # Note: lxml supports pretty_print mode, but it doesn't do what we want anyway
+        self.tree.write(f, encoding='UTF-8', xml_declaration=True)
+    
+    def update(self,args):
+        if args.mol5d_pairwise:
+            self.enable_mol5d_pairwise()
+        while self.schema_ver < args.target_version:
             func_name = 'translate_' + str(self.schema_ver) + '_to_' + str(self.schema_ver+1)
             if not func_name in dir(self):      # do we have this function?
                 raise NotImplementedError('Translation function '+func_name)
@@ -282,6 +319,40 @@ class Translator:
                             child.append(ET.Element('positive',{'id':comp_id}))
                         for comp_id in negComponents:
                             child.append(ET.Element('negative',{'id':comp_id}))
+    
+    def enable_mol5d_pairwise(self):
+        """Enable the Molineaux model and pairwise sampling while preserving a
+        5-day step. Written to operate on v33 XMLs but should work on other versions."""
+        if self.interval != 5:
+            # changing interval implies varies times/delays might need changing
+            raise DocumentError('Expected interval to be 5 when enabling Molineax 5-day mode')
+        model = self.root.find('model')
+        mOpts = model.find('ModelOptions')
+        have_set_mort_fix = False
+        have_set_mol = False
+        have_set_pairwise = False
+        for opt in mOpts.findall('option'):
+            name = opt.get('name')
+            if name == 'MAX_DENS_CORRECTION':
+                mOpts.remove(opt)       # option no longer needed
+            elif name == 'INNATE_MAX_DENS':
+                mOpts.remove(opt)       # option no longer needed
+            elif name == 'INDIRECT_MORTALITY_FIX':
+                opt.set('value', 'true')        # preferably we should use this
+                have_set_mort_fix = True
+            elif name == 'MOLINEAUX_WITHIN_HOST_MODEL':
+                opt.set('value', 'true')
+                have_set_mol = True
+            elif name == 'MOLINEAUX_PAIRWISE_SAMPLE':
+                opt.set('value', 'true')
+                have_set_pairwise = True
+        if not have_set_mort_fix:
+            # As of version 33 (possibly earlier), this is enabled by default anyway
+            mOpts.append(ET.Element('option', {'name':'INDIRECT_MORTALITY_FIX', 'value':'true'}))
+        if not have_set_mol:
+            mOpts.append(ET.Element('option', {'name':'MOLINEAUX_WITHIN_HOST_MODEL', 'value':'true'}))
+        if not have_set_pairwise:
+            mOpts.append(ET.Element('option', {'name':'MOLINEAUX_PAIRWISE_SAMPLE', 'value':'true'}))
 
 def die(*args):
     print(*args, file=sys.stderr)
@@ -300,17 +371,65 @@ def readArgs():
                         help='Destination directory for translated scenarios')
     group.add_argument('-i', '--in-place', action='store_true', default=False,
                         help='If set, files will be updated in-place (incompatible with -d option')
+    group.add_argument('--db', metavar='DBNAME', action='store', default=None,
+                       help='If set, files will be updated from the scenarios table of this database.')
+    parser.add_argument('-u','--user', metavar='USER', action='store', default=None,
+                       help='Username to connect to database with. If not set, will try to connect without username.')
+    parser.add_argument('-r','--run-id', metavar='RUN_ID', action='store', default=None,
+                        help='[DB mode] If given, only scenarios with this run_id are updated; if not, all are updated.')
+    parser.add_argument('--mol5d-pairwise', action='store_true', default=False,
+                        help='Update XMLs using a 5-day time step to use the Molineaux model with pairwise sampling.')
     args = parser.parse_args()
     if args.dest and not os.path.isdir(args.dest):
         die('Not a directory:', args.dest)
+    if args.db == None:
+        if args.run_id != None:
+            die('-r / --run-id argument not applicable if not using database (--db)')
+        if len(args.files) == 0:
+            die('Not in database mode and no files to translate')
     return args
 
-def main():
-    args = readArgs()
-    # args.target_version : target version number
-    # args.files : list of files to translate
-    # args.dest : destination directory or None
-    # args.in_place : overwrite input files (True) or not (False)
+def updateDB(args):
+    if args.user == None:
+        db = MySQLdb.connect("localhost", db=args.db)
+    else:
+        print('Password for database user '+args.user+'?')
+        passwd = getpass.getpass()
+        db = MySQLdb.connect("localhost", db=args.db,
+                             user=args.user, passwd=passwd)
+    
+    cur = db.cursor() 
+    
+    statement_base = """SELECT s.id,s.run_id,p.interval_,s.xml
+FROM scenarios s JOIN parameterizations p ON s.run_id=p.run_id"""
+    group_by = ' GROUP BY s.id,s.run_id'
+    if args.run_id == None:
+        cur.execute(statement_base + group_by)
+    else:
+        cur.execute(statement_base + ' WHERE s.run_id='+str(args.run_id) + group_by)
+    changed=[] # tuples (s.id, s.run_id, s.xml)
+    nErrors=0
+    for row in cur.fetchall() : 
+        print('Updating XML for run '+str(row[1])+', scenario '+str(row[0])+' (interval: '+str(row[2])+')')
+        xml = row[3]
+        try:
+            translator = Translator(text=xml,interval=row[2])
+            translator.update(args)
+            changed.append((row[0],row[1],translator.to_string()))
+        except DocumentError,e:
+            for line in e.lines:
+                print('Error:',line)
+            nErrors += 1
+            if nErrors > 5:
+                die('Stopping: too many errors')
+    for row in changed:
+        cur.execute('UPDATE scenarios set xml=%s where id=%s and run_id=%s', (row[2], row[0], row[1]))
+        print('Updated XML with id', row[0], 'run_id', row[1])
+    db.commit()
+    db.close()
+    print('Changes committed')
+
+def updateFiles(args):
     for f_path in args.files:
         try:
             if not os.path.isfile(f_path):
@@ -318,7 +437,7 @@ def main():
             with open(f_path, 'r') as f:
                 translator = Translator(f)
             print('Translating',f_path,'from',translator.schema_ver,'to',args.target_version)
-            translator.update_to(args.target_version)
+            translator.update(args)
             if args.in_place:
                 with open(f_path, 'w') as f:
                     translator.write_to(f)
@@ -329,6 +448,23 @@ def main():
         except DocumentError,e:
             for line in e.lines:
                 print('Error:',line)
+
+def main():
+    args = readArgs()
+    # args.target_version : target version number
+    # args.files : list of files to translate
+    # args.dest : destination directory or None
+    # args.in_place : overwrite input files (True) or not (False)
+    # args.db : database name or None
+    # args.user : database username or None
+    # args.run_id : run_id for scenarios in DB
+    # args.mol5d_pairwise : switch WHM, keep interval
+    if args.db != None:
+        if len(args.files) > 0:
+            die('incompatible: DB mode and specifying files on the commandline')
+        updateDB(args)
+    else:
+        updateFiles(args)
 
 if __name__ == "__main__":
     main()
