04642b7d07d0b6e9b0769666457c65114cd6444f
Diggory Hardy <diggory.hardy@unibas.ch>
2014-12-11 17:18:30 +0100

    Update copyright information of Python scripts

--- util/plotResult.py
+++ util/plotResult.py
@@ -1,23 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+"""
+This file is part of OpenMalaria.
 
-# This file is part of OpenMalaria.
-# 
-# Copyright (C) 2005-2011 Swiss Tropical Institute and Liverpool School Of Tropical Medicine
-# 
-# OpenMalaria is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or (at
-# your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+Copyright (C) 2005-2014 Swiss Tropical Institute
+
+OpenMalaria is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+"""
 
 from optparse import OptionParser
 import numpy
