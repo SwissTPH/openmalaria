f728817cc9f43a671cfa1d67d8e29e214178f790
Diggory Hardy <diggory.hardy@unibas.ch>
2015-02-11 10:38:35 +0100

    Update licence headers

--- model/SimTime.h
+++ model/SimTime.h
@@ -1,7 +1,7 @@
 /* This file is part of OpenMalaria.
  * 
- * Copyright (C) 2005-2014 Swiss Tropical and Public Health Institute
- * Copyright (C) 2005-2014 Liverpool School Of Tropical Medicine
+ * Copyright (C) 2005-2015 Swiss Tropical and Public Health Institute
+ * Copyright (C) 2005-2015 Liverpool School Of Tropical Medicine
  * 
  * OpenMalaria is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
