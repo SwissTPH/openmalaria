6733370d544d6be3888c003e1bd189883151053e
Tobias Thüring <tobias.thuering@unibas.ch>
2015-08-05 12:52:42 +0200

    Make build-info generate actual JSON output

--- util/build-info.sh
+++ util/build-info.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-echo -e "[$TRAVIS_JOB_ID : {" \
-      "build: $BASE_URL/builds/$TRAVIS_BUILD_ID,\n" \
-      "commit: $BASE_URL/commits/$TRAVIS_BRANCH/$TRAVIS_COMMIT,\n" \
-      "job: $BASE_URL/jobs/$TRAVIS_JOB_ID,\n" \
-      "os: $TRAVIS_OS_NAME,\n" \
-      "tag: $TRAVIS_TAG\n" \
-      "}]"
+if [ -z "$TRAVIS_BUILD_ID" ]
+then
+	echo "Error: Use only for automated Travis-ci builds"  1>&2
+	exit 1
+fi
+
+BUILD=$BASE_URL'/builds/'$TRAVIS_BUILD_ID
+COMMIT=$BASE_URL'/commits/'$TRAVIS_BRANCH'/'$TRAVIS_COMMIT
+JOB=$BASE_URL'/jobs/'$TRAVIS_JOB_ID
+OS=$TRAVIS_OS_NAME
+TAG=$TRAVIS_TAG
+
+echo -e '{ "'$TRAVIS_JOB_ID'" : {' \
+      '"build" : "'$BUILD'", '"\n" \
+      '"commit" : "'$COMMIT'", '"\n" \
+      '"job" : "'$JOB'",'"\n" \
+      '"os" : "'$OS'",'"\n" \
+      '"tag" : "'$TAG'"'"\n" \
+      '}}'
