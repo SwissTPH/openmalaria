fe5eda5c533bff7a4f0bfd79fb52480b1054826d
Tobias Thüring <tobias.thuering@unibas.ch>
2015-08-21 12:07:43 +0200

    Use rebuilt libraries (msbuild v140)

--- util/install-dep-windows/install-dependencies-win.bat
+++ util/install-dep-windows/install-dependencies-win.bat
@@ -16,16 +16,15 @@ dir %install%\boost*\*
 
 mkdir %install%\lib
 REM Installing gsl libs
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\gsl-libs.ps1' -dir %install% -src 'https://github.com/tph-thuering/gsl/releases/download/gsl/gsl-libs.zip'"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\gsl-libs.ps1' -dir %install% -src 'https://github.com/tph-thuering/gsl/releases/download/0.0.10/gsl-libs.zip'"
 
 mkdir %install%\gsl\gsl\
 REM Installing gsl headers
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\gsl-headers.ps1' -dir %install% -src 'https://github.com/tph-thuering/gsl/releases/download/gsl/gsl-headers.zip'"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\gsl-headers.ps1' -dir %install% -src 'https://github.com/tph-thuering/gsl/releases/download/0.0.10/gsl-headers.zip'"
 
 REM Installing zlib
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\zlib.ps1' -dir %install% -src 'https://github.com/tph-thuering/zlib/releases/download/zlib/zlib.lib.zip'"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\zlib.ps1' -dir %install% -src 'https://github.com/tph-thuering/zlib/releases/download/0.0.5/zlib.lib.zip'"
 
 REM Installing xsd + xerces-c
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\xsd.ps1' -dir %install% -src 'http://www.codesynthesis.com/download/xsd/4.0/windows/i686/xsd-4.0.msi'"
-
 
