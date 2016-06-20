36f57922378ea437614dbf6b4bc60946f1386537
Diggory Hardy <git@dhardy.name>
2016-06-20 15:38:42 +0100

    Drug conversion: remove "correction factor"
    This was added to make results match LSTM's results. Why it's
    needed is the question.

--- model/PkPd/Drug/LSTMDrugConversion.cpp
+++ model/PkPd/Drug/LSTMDrugConversion.cpp
@@ -111,10 +111,7 @@ double calculateMetaboliteDrugFactor( struct Params_convFactor p, double expAbso
     const double qtyM = calculateMetaboliteQuantity(p, p.qtyG, p.qtyP, p.qtyM, expAbsorb, expPLoss, t);
     const double cM = qtyM * p.invVdM;              // concentrations; mg/l
     const double cnM = pow(cM, p.nM);               // (mg/l) ^ n
-    //FIXME:
-    // correctional factor tries to fix results for metabolite quantity calculation
-    const double correction_factor = 0.990718;
-    const double fCM = correction_factor * p.VM * cnM / (cnM + p.KnM);  // unitless
+    const double fCM = p.VM * cnM / (cnM + p.KnM);  // unitless
     return fCM;
 }
 
