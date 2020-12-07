32673cadc3db4bd0eb88817b1f8bf83f54a2614b
Aurélien Cavelan <aurelien.cavelan@unibas.ch>
2020-12-07 14:50:23 +0100

    merge EmergenceModel cpp into header

--- model/Transmission/Anopheles/EmergenceModel.h
+++ model/Transmission/Anopheles/EmergenceModel.h
@@ -27,13 +27,18 @@
 #include "util/checkpoint_containers.h"
 #include "util/vecDay.h"
 
+#include <cmath>
+
+#include "util/vectors.h"
+#include "util/CommandLine.h"
+#include "util/errors.h"
+
 namespace OM {
 namespace Transmission {
 namespace Anopheles {
 
 using namespace std;
-using util::vecDay;
-using util::LocalRng;
+using namespace OM::util;
 
 // forward declare to avoid circular dependency:
 class MosqTransmission;
@@ -49,7 +54,16 @@ class EmergenceModel
 public:
     ///@brief Initialisation and destruction
     //@{
-    EmergenceModel();
+    EmergenceModel() :
+            EIRRotateAngle(numeric_limits<double>::quiet_NaN()),
+            FSRotateAngle(numeric_limits<double>::quiet_NaN()),
+            initNvFromSv(numeric_limits<double>::quiet_NaN()),
+            initOvFromSv(numeric_limits<double>::quiet_NaN()),
+            emergenceSurvival(1.0)
+    {
+        forcedS_v.resize (SimTime::oneYear());
+    }
+
     virtual ~EmergenceModel() {}
     
     /** Called to initialise life-cycle parameters from XML data.
@@ -66,17 +80,123 @@ public:
      * @param initialisationEIR In/out parameter: TransmissionModel::initialisationEIR
      * @param EIPDuration parameter from MosqTransmission (used for an estimation)
      */
-    void initEIR(
-        const scnXml::AnophelesParams& anoph,
-        vector<double>& initialisationEIR,
-        SimTime EIPDuration );
+    void initEIR(const scnXml::AnophelesParams& anoph, vector<double>& initialisationEIR, SimTime EIPDuration)
+    {
+        const scnXml::Seasonality& seasonality = anoph.getSeasonality();
+        if ( seasonality.getInput() != "EIR" ) {
+            throw util::xml_scenario_error("entomology.anopheles.seasonality.input: must be EIR (for now)");
+            //TODO
+        }
+        // EIR for this species, with index 0 refering to value over first interval
+        vecDay<double> speciesEIR (SimTime::oneYear());
+
+        if ( seasonality.getFourierSeries().present() ) {
+            const scnXml::FourierSeries& seasFC = seasonality.getFourierSeries().get();
+            const scnXml::FourierSeries::CoefficSequence& fsCoeffic = seasFC.getCoeffic();
+
+            FSCoeffic.reserve (2*fsCoeffic.size() + 1);
+
+            FSCoeffic.push_back( 0.0 );     // value doesn't matter; EIR will be scaled
+            for( auto it=fsCoeffic.begin(); it!=fsCoeffic.end(); ++it ) {
+                FSCoeffic.push_back( it->getA() );
+                FSCoeffic.push_back( it->getB() );
+            }
+            // According to spec, EIR for first day of year (rather than EIR at the
+            // exact start of the year) is generated with t=0 in Fourier series.
+            EIRRotateAngle = seasFC.getEIRRotateAngle();
+        } else if ( seasonality.getMonthlyValues().present() ) {
+            const scnXml::MonthlyValues& seasM = seasonality.getMonthlyValues().get();
+            if ( seasM.getSmoothing() != "fourier" ) {
+                throw util::xml_scenario_error("entomology.anopheles.seasonality.monthlyValues.smoothing: only fourier supported at the moment");
+                //TODO: should be easy to add no smoothing
+            }
+
+            const size_t N_m = 12;
+            const scnXml::MonthlyValues::ValueSequence seq = seasM.getValue();
+            assert( seq.size() == N_m );    // enforced by schema
+            vector<double> months(N_m);
+            double sum = 0.0;
+            for( size_t i = 0; i < N_m; ++i ) {
+                months[i] = seq[i];
+                sum += months[i];
+            }
+            // arbitrary minimum we allow (cannot have zeros since we take the logarithm)
+            double min = sum/1000.0;
+            for( size_t i = 0; i < N_m; ++i ) {
+                if ( months[i] < min )
+                    months[i] = min;
+            }
+
+            FSCoeffic.assign( 5, 0.0 );
+            //TODO: determine whether to use Fourier Series Coefficient method or
+            // Discrete Fourier Transform. Former is designed for integrals (which
+            // roughly what we have?), the latter for discrete values. DFT doesn't
+            // work well when number of intervals changes?
+            vectors::logFourierCoefficients(months, FSCoeffic);
+
+            // The above places the value for the first month at angle 0, so
+            // effectively the first month starts at angle -2*pi/24 radians.
+            // The value for the first day of the year should start 2*pi/(365*2)
+            // radians later, so adjust EIRRotateAngle to compensate.
+
+            // Change this to 0 later?
+            EIRRotateAngle = M_PI * ( 1.0/12.0 - 1.0/365.0 );
+        } else {
+            assert( seasonality.getDailyValues().present() );      // XML loading code should enforce this
+            throw util::xml_scenario_error("entomology.anopheles.seasonality.dailyValues: not supported yet");
+            //TODO: can we encode as a Fourier series without smoothing?
+        }
+
+        if ( !seasonality.getAnnualEIR().present() ) {
+            //TODO: work out when this is not required and implement code
+            throw util::xml_scenario_error("entomology.anopheles.seasonality.annualEIR is required at the moment");
+        }
+        double targetEIR = seasonality.getAnnualEIR().get();
+        
+        // Now we rescale to get an EIR of targetEIR.
+        // Calculate current sum as is usually done.
+        vectors::expIDFT (speciesEIR, FSCoeffic, EIRRotateAngle);
+        // And scale (also acts as a unit conversion):
+        FSCoeffic[0] += log( targetEIR / vectors::sum( speciesEIR ) );
+        
+        // Calculate forced EIR for pre-intervention phase from FSCoeffic:
+        vectors::expIDFT (speciesEIR, FSCoeffic, EIRRotateAngle);
+        
+        // Add to the TransmissionModel's EIR, used for the initalization phase.
+        // Note: sum stays the same, units changes to per-time-step.
+        for( SimTime i = SimTime::zero(); i < SimTime::oneYear(); i += SimTime::oneDay() ){
+            // index 0 of initialisationEIR corresponds to first period of year
+            initialisationEIR[mod_nn(i.inSteps(), sim::stepsPerYear())] += speciesEIR[i];
+        }
+        
+        if ( util::CommandLine::option( util::CommandLine::PRINT_ANNUAL_EIR ) ) {
+            cout << "Annual EIR for "<<anoph.getMosquito()
+                 << ": "<<vectors::sum( speciesEIR )<<endl;
+        }
+
+        // Set other data used for mosqEmergeRate calculation:
+        FSRotateAngle = EIRRotateAngle - (EIPDuration.inDays()+10)/365.*2.*M_PI;       // usually around 20 days; no real analysis for effect of changing EIPDuration or mosqRestDuration
+        initNvFromSv = 1.0 / anoph.getPropInfectious();
+        initOvFromSv = initNvFromSv * anoph.getPropInfected();
+    }
     
     /** Set up the non-host-specific interventions. */
-    void initVectorInterv( const scnXml::VectorSpeciesIntervention& elt, size_t instance );
+    void initVectorInterv( const scnXml::VectorSpeciesIntervention& elt, size_t instance ){
+        if( emergenceReduction.size() <= instance )
+            emergenceReduction.resize( instance+1 );
+        
+        if( elt.getEmergenceReduction().present() ){
+            const scnXml::EmergenceReduction& elt2 = elt.getEmergenceReduction().get();
+            if( elt2.getInitial() > 1.0 )
+                throw util::xml_scenario_error( "emergenceReduction intervention: initial effect must be ≤ 1" );
+            emergenceReduction[instance].set (elt2.getInitial(), elt2.getDecay(), "emergenceReduction");
+        }
+    }
+
     
     /** Scale the internal EIR representation by factor; used as part of
      * initialisation. */
-    void scaleEIR( double factor );
+    void scaleEIR( double factor ) { FSCoeffic[0] += log( factor ); }
     
     /** Latter part of AnophelesModel::init2.
      *
@@ -97,7 +217,13 @@ public:
     
     /// Update per time-step (for larviciding intervention). Call before
     /// getting emergence each time-step.
-    void update ();
+    void update ()
+    {
+        emergenceSurvival = 1.0;
+        for( size_t i = 0; i < emergenceReduction.size(); ++i ){
+            emergenceSurvival *= 1.0 - emergenceReduction[i].current_value( sim::ts0() );
+        }
+    }
     
     /** Model updates.
      * 
@@ -117,7 +243,11 @@ public:
     ///@brief Interventions and reporting
     //@{
     /// Start an intervention affecting the vector population.
-    void deployVectorPopInterv (LocalRng& rng, size_t instance);
+    void deployVectorPopInterv (LocalRng& rng, size_t instance)
+    {
+        assert( instance < emergenceReduction.size() );
+        emergenceReduction[instance].deploy( rng, sim::now() );
+    }
     
     virtual double getResAvailability() const =0;
     virtual double getResRequirements() const =0;
