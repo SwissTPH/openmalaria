9515a7f3458f86115a4d600e21905a65477c0a1c
Aurélien Cavelan <aurelien.cavelan@unibas.ch>
2022-02-25 16:39:32 +0100

    add vaccine tests and bump version to 44.0

--- model/util/DocumentLoader.h
+++ model/util/DocumentLoader.h
@@ -35,7 +35,7 @@ namespace OM { namespace util {
 class DocumentLoader {
 public:
     /// Current schema version.
-    static const int SCHEMA_VERSION = 43;
+    static const int SCHEMA_VERSION = 44;
     
     DocumentLoader () : documentChanged(false) {}
     
