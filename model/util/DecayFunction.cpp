f292cad21fb7e90e0a78bcee3ec03b97e3943308
Aurélien Cavelan <aurelien.cavelan@unibas.ch>
2023-05-23 10:43:42 +0200

    rename complement to increasing

--- model/util/DecayFunction.cpp
+++ model/util/DecayFunction.cpp
@@ -36,7 +36,7 @@ class BaseHetDecayFunction : public DecayFunction {
     LognormalSampler het;
 public:
     BaseHetDecayFunction( double timeFactorHet, const scnXml::DecayFunction& elt ) :
-        DecayFunction( elt.getComplement() )
+        DecayFunction( elt.getIncreasing() )
     {
         het.setMeanCV( 1.0, elt.getCV() );
     }
@@ -44,11 +44,9 @@ public:
     virtual unique_ptr<DecayFunction> _sample(double hetFactor) const =0;
 
     unique_ptr<DecayFunction> hetSample (LocalRng& rng) const{
-        //return DecayFunction(het.sample(rng) * getBaseTimeFactorHet());
         return _sample(het.sample(rng));
     }
     unique_ptr<DecayFunction> hetSample (NormalSample sample) const{
-        //return DecayFunction(het.sample(sample) * getBaseTimeFactorHet());
         return _sample(het.sample(sample));
     }
 };
