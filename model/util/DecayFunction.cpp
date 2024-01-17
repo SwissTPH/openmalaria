c7f18777df3e60de78a4b0012f5c572ed543af68
Aurélien Cavelan <aurelien.cavelan@unibas.ch>
2024-01-17 07:52:42 +0100

    rebase

--- model/util/DecayFunction.cpp
+++ model/util/DecayFunction.cpp
@@ -106,7 +106,6 @@ public:
     {}
     
     double compute(double effectiveAge) const{
-        cout << "Linear " << effectiveAge << " " << invL << " " << hetFactor <<endl;
         if( effectiveAge * invL * hetFactor < 1.0 ){
             return 1.0 - effectiveAge * invL * hetFactor;
         }else{
