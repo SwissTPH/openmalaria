0a4e94e1cd2ef637c56a0450f7c92fa5914b892e
b-s-code <89502375+b-s-code@users.noreply.github.com>
2025-05-14 16:12:14 +0800

    Parameters class compiles. Clients of the class no longer compile. This is because the namespace promotion offered by raw enums has now been taken away.

--- model/Parameters.h
+++ model/Parameters.h
@@ -23,179 +23,283 @@
 #define Hmod_InputParameters
 
 #include <map>
+#include <optional>
+#include <unordered_map>
 #include <schema/scenario.h>
 #include "util/errors.h"
 
 namespace scnXml { class Parameters; }
 namespace OM {
 
+/*
+* Each "parameter" can be thought of as a 3-tuple.
+*
+* A parameter has a "code" or an "index", which is the positive integer that identifies the
+* given parameter in the input XML.
+*
+* Each parameter has a "name", which is used by clients of this class to refer to the parameter.
+* Parameter names strings in an input XML file are simply for user convenience.  The values of the
+* "name" attributes in XML are *completely* ignored by OpenMalaria itself.
+*
+* Finally, each parameter can hold an assigned value, of type double.  An *optional* double is used
+* to represent each parameter's value because many scenarios will specify values for less than the
+* full set of parameters.
+*
+* Note that, when a client of this class attempts to access the value of some parameter which did
+* not actually get a value assigned to it, it is considered a run time error.  This class is
+* responsible for handling such errors.
+*
+*          .----------------------------------------------------------------------.
+*          | Part of 3-tuple                                                      |
+*          .------------------------.---------------------.-----------------------.
+*          | Parameter Code         | Parameter Name      | Parameter Value       |
+* .--------.------------------------.---------------------.-----------------------.
+* | type   | (int)                  | enum class          | std::optional<double> |
+* .--------.------------------------.---------------------.-----------------------.
+* | notes  | The sequence of para-  | Used by clients of  | Has a value iff input |
+* |        | meter codes may be     | this class to       | XML sets one or input |
+* |        | non-contiguous.        | identify the param. | XML selects a model   |
+* |        | non-contiguous.        | identify the param. | which sets a value    |
+* |        |                        |                     | for this param.       |
+* `--------`------------------------`---------------------`-----------------------`
+*
+*/
 class Parameters {
 public:
+
     /*
-    * This defines all possible parameters.
-    * Order here is not important.
+    * Defines the names that clients of this class use in order to reference
+    * the values of parameters.
+    *
+    * NOTE: any time that a new parameter is added here, corresponding entries
+    * *must* be added to idCodeToNameMap and nameToValueMap.
     */
-    enum class Parameter {
+    enum class ParameterName {
+        /// @b Infection incidence model parameters
+        //@{
         NEG_LOG_ONE_MINUS_SINF,
         E_STAR,
         SIMM,
         X_STAR_P,
         GAMMA_P,
-        SIGMA_I_SQ,
+        //@}
+        /// @b Immunity parameters, mostly on infections
+        //@{
+        SIGMA_I_SQ,                 ///< Host (not infection) parameter
         CUMULATIVE_Y_STAR,
         CUMULATIVE_H_STAR,
         NEG_LOG_ONE_MINUS_ALPHA_M,
         DECAY_M,
+        //@}
+        /// @b DescriptiveInfection specific
+        //@{
         SIGMA0_SQ,
         X_NU_STAR,
+        //@}
+        /// @b Used in PathogenesisModel
+        //@{
         Y_STAR_SQ,
         ALPHA,
-        DENSITY_BIAS_NON_GARKI,
-        BASELINE_AVAILABILITY_SHAPE,
-        LOG_ODDS_RATIO_CF_COMMUNITY,
-        INDIRECT_RISK_COFACTOR,
-        NON_MALARIA_INFANT_MORTALITY,
-        DENSITY_BIAS_GARKI,
-        SEVERE_MALARIA_THRESHHOLD,
-        IMMUNITY_PENALTY,
-        IMMUNE_EFFECTOR_DECAY,
+        //@}
+        DENSITY_BIAS_NON_GARKI,        ///< Used in Diagnostic
+        BASELINE_AVAILABILITY_SHAPE,   ///< Used in InfectionIncidenceModel
+        LOG_ODDS_RATIO_CF_COMMUNITY,   ///< Used in CaseManagementModel
+        INDIRECT_RISK_COFACTOR,        ///< Used in PathogenesisModel
+        NON_MALARIA_INFANT_MORTALITY,  ///< Used in Summary
+        DENSITY_BIAS_GARKI,            ///< Used in Diagnostic
+        SEVERE_MALARIA_THRESHHOLD,     ///< Used in PathogenesisModel
+        IMMUNITY_PENALTY,              ///< Used in WHFalciparum
+        IMMUNE_EFFECTOR_DECAY,         ///< Used in WHFalciparum
+        /// @b Used in PathogenesisModel
+        //@{
         COMORBIDITY_INTERCEPT,
         Y_STAR_HALF_LIFE,
         Y_STAR_1,
-        ASEXUAL_IMMUNITY_DECAY,
+        //@}
+        ASEXUAL_IMMUNITY_DECAY,        ///< Used in WHFalciparum
+        /// @b Used in PathogenesisModel
+        //@{
         Y_STAR_0,
+
         CRITICAL_AGE_FOR_COMORBIDITY,
         MUELLER_RATE_MULTIPLIER,
         MUELLER_DENSITY_EXPONENT,
+        //@}
+        /// EventScheduler: v in "Case Fatality Rate proposal"
         CFR_SCALE_FACTOR,
+
+        /// @b Molineaux: sampling parameters (not pairwise mode only)
         MEAN_LOCAL_MAX_DENSITY,
         SD_LOCAL_MAX_DENSITY,
         MEAN_DIFF_POS_DAYS,
         SD_DIFF_POS_DAYS,
-        CFR_NEG_LOG_ALPHA
+
+        /// EventScheduler: exp(-CFR_NEG_LOG_ALPHA) is the proportion of deaths occuring on the first day
+        CFR_NEG_LOG_ALPHA,
     };
 
-    /*
-    * Initializes parameters using what is specified in the input XML, and does some
-    * validation on the specified parameters and values.
-    */
     Parameters( const scnXml::Parameters& parameters )
     {
-        const scnXml::Parameters::ParameterSequence& paramSeq = parameters.getParameter();
-        for(auto iter = paramSeq.begin(); iter != paramSeq.end(); ++iter) {
-            const int paramId = iter->getNumber();
-            const double paramValue = iter->getValue();
-
-            // C++20 offers "contains" which would be more expressive than "count" here.
-            const bool paramIdIsValid = IdToParamMap.count(paramID);
-            const bool paramIsAlreadyDefined = parameterValues.count(paramID);
-            if (!paramIdIsValid)
-            {
-                throw util::xml_scenario_error("index " + to_string(paramID) +
-                        " does not correspond to any parameter")
-            }
-            else if (paramIsAlreadyDefined)
-            {
-                throw util::xml_scenario_error("parameter with index " + to_string(parameter) +
-                        " described twice");
-            }
-            else
-            {
-                Parameter targetParam = IdToParamMap[paramID];
-                paramValues[targetParam] = parameterValue;
-            }
-        }
+        initializeParamsFromXML(parameters);
     }
 
     /**
      * Get a parameter, using one of the Parameter codes.
      */
-    double operator[]( Parameter parameter )const{
-        auto iter = parameterValues.find( parameter );
-        if( iter == parameterValues.end() )
-            throw util::xml_scenario_error("parameter " + to_string(parameter) +
-                    " required but not described");
-        return iter->second;
+    double operator[]( ParameterName name )const{
+        // First check the parameter actually exists and has a value.
+        const bool paramHasAValueSet = nameToValueMap.at(name) != std::nullopt;
+        if (!paramHasAValueSet)
+        {
+            for (std::pair<int, ParameterName> pair : idCodeToNameMap)
+            {
+                if (pair.second == name)
+                {
+                    const int paramCode = pair.first;
+                    throw util::xml_scenario_error("Parameter with index " + to_string(paramCode) +
+                            " required but not described.");
+                }
+            }
+            throw util::base_exception("A parameter required by this simulation is missing a definition fo r its ID in C++ code");
+        }
+
+        return nameToValueMap.at(name).value();
     }
 
 private:
-    // Initialized (derived) values:
-    std::map<Parameter, double> parameterValues;
 
     /*
-    * When a user sets a parameter's value in XML, the important things for them to specify are
-    * the integer ID of the parameter, and the value they want the parameter to have.
-    * This map defines the relation between such integer IDs and actual parameters.
+    * Initializes parameters using explicit values specified in the input XML,
+    * and does some validation on the specified parameters and values.
+    * This method will be used if the user does not use the "base" model or any
+    * other pre-set collection of parameter values.
     */
-    std::map<int, Parameter> IdToParamMap {
-        /// @b Infection incidence model parameters
-        //@{
-        { 1, NEG_LOG_ONE_MINUS_SINF },
-        { 2, E_STAR },
-        { 3, SIMM },
-        { 4, X_STAR_P },
-        { 5, GAMMA_P },
+    void initializeParamsFromXML(const scnXml::Parameters& parameters)
+    {
+        const scnXml::Parameters::ParameterSequence& paramSeq = parameters.getParameter();
+        for(auto iter = paramSeq.begin(); iter != paramSeq.end(); ++iter)
+        {
+            const int paramId = iter->getNumber();
+            const double paramValueFromXML = iter->getValue();
 
-        //@}
-        /// @b Immunity parameters, mostly on infections
-        //@{
-        { 6, SIGMA_I_SQ },                 ///< Host (not infection) parameter
-        { 7, CUMULATIVE_Y_STAR },
-        { 8, CUMULATIVE_H_STAR },
-        { 9, NEG_LOG_ONE_MINUS_ALPHA_M },
-        { 10, DECAY_M },
+            // C++20 offers "contains" which would be more expressive than "count" here.
+            const bool paramIdIsValid = idCodeToNameMap.count(paramId);
+            if (!paramIdIsValid)
+            {
+                throw util::xml_scenario_error("The specified parameter code/index "
+                        + to_string(paramId) +
+                        " does not correspond to any actual parameter.");
+            }
+            const ParameterName nameOfParamToSet = idCodeToNameMap.at(paramId);
 
-        //@}
-        /// @b DescriptiveInfection specific
-        //@{
-        { 11, SIGMA0_SQ },
-        { 12, X_NU_STAR },
+            // C++20 offers "contains" which would be more expressive than "count" here.
+            const bool paramValueAlreadySet = nameToValueMap.at(nameOfParamToSet) != std::nullopt;
+            if (paramValueAlreadySet)
+            {
+                throw util::xml_scenario_error("Parameter with index " + to_string(paramId) +
+                        " described twice.");
+            }
 
-        //@}
-        /// @b Used in PathogenesisModel
-        //@{
-        { 13, Y_STAR_SQ },
-        { 14, ALPHA },
-        //@}
+            nameToValueMap[nameOfParamToSet] = paramValueFromXML;
+        }
+    }
 
-        { 15, DENSITY_BIAS_NON_GARKI },        ///< Used in Diagnostic
-        { 16, BASELINE_AVAILABILITY_SHAPE },   ///< Used in InfectionIncidenceModel
-        { 17, LOG_ODDS_RATIO_CF_COMMUNITY },   ///< Used in CaseManagementModel
-        { 18, INDIRECT_RISK_COFACTOR },        ///< Used in PathogenesisModel
-        { 19, NON_MALARIA_INFANT_MORTALITY },  ///< Used in Summary
-        { 20, DENSITY_BIAS_GARKI },            ///< Used in Diagnostic
-        { 21, SEVERE_MALARIA_THRESHHOLD },     ///< Used in PathogenesisModel
-        { 22, IMMUNITY_PENALTY },              ///< Used in WHFalciparum
-        { 23, IMMUNE_EFFECTOR_DECAY },         ///< Used in WHFalciparum
+    /*
+    * Defines the map from parameter ID numbers (AKA parameter codes) to parameter names.
+    *
+    * The keys of this map may not form a contiguous sequence, e.g. in the case where a
+    * parameter gets deleted altogether from the simulation code.  This is okay, and is
+    * in fact already the case at the time of writing.
+    */
+    const std::unordered_map<int, ParameterName> idCodeToNameMap
+    {
+        { 1,  ParameterName::NEG_LOG_ONE_MINUS_SINF },
+        { 2,  ParameterName::E_STAR },
+        { 3,  ParameterName::SIMM },
+        { 4,  ParameterName::X_STAR_P },
+        { 5,  ParameterName::GAMMA_P },
+        { 6,  ParameterName::SIGMA_I_SQ },
+        { 7,  ParameterName::CUMULATIVE_Y_STAR },
+        { 8,  ParameterName::CUMULATIVE_H_STAR },
+        { 9,  ParameterName::NEG_LOG_ONE_MINUS_ALPHA_M },
+        { 10, ParameterName::DECAY_M },
+        { 11, ParameterName::SIGMA0_SQ },
+        { 12, ParameterName::X_NU_STAR },
+        { 13, ParameterName::Y_STAR_SQ },
+        { 14, ParameterName::ALPHA },
+        { 15, ParameterName::DENSITY_BIAS_NON_GARKI },
+        { 16, ParameterName::BASELINE_AVAILABILITY_SHAPE },
+        { 17, ParameterName::LOG_ODDS_RATIO_CF_COMMUNITY },
+        { 18, ParameterName::INDIRECT_RISK_COFACTOR },
+        { 19, ParameterName::NON_MALARIA_INFANT_MORTALITY },
+        { 20, ParameterName::DENSITY_BIAS_GARKI },
+        { 21, ParameterName::SEVERE_MALARIA_THRESHHOLD },
+        { 22, ParameterName::IMMUNITY_PENALTY },
+        { 23, ParameterName::IMMUNE_EFFECTOR_DECAY },
+        { 24, ParameterName::COMORBIDITY_INTERCEPT },
+        { 25, ParameterName::Y_STAR_HALF_LIFE },
+        { 26, ParameterName::Y_STAR_1 },
+        { 27, ParameterName::ASEXUAL_IMMUNITY_DECAY },
+        { 28, ParameterName::Y_STAR_0 },
+        { 30, ParameterName::CRITICAL_AGE_FOR_COMORBIDITY },
+        { 31, ParameterName::MUELLER_RATE_MULTIPLIER },
+        { 32, ParameterName::MUELLER_DENSITY_EXPONENT },
+        { 33, ParameterName::CFR_SCALE_FACTOR },
+        { 34, ParameterName::MEAN_LOCAL_MAX_DENSITY },
+        { 35, ParameterName::SD_LOCAL_MAX_DENSITY },
+        { 36, ParameterName::MEAN_DIFF_POS_DAYS },
+        { 37, ParameterName::SD_DIFF_POS_DAYS },
+        { 38, ParameterName::CFR_NEG_LOG_ALPHA }
+    };
 
-        /// @b Used in PathogenesisModel
-        //@{
-        { 24, COMORBIDITY_INTERCEPT },
-        { 25, Y_STAR_HALF_LIFE },
-        { 26, Y_STAR_1 },
-        //@}
-
-        { 27, ASEXUAL_IMMUNITY_DECAY },        ///< Used in WHFalciparum
-
-        /// @b Used in PathogenesisModel
-        //@{
-        { 28, Y_STAR_0 },
-        { 30, CRITICAL_AGE_FOR_COMORBIDITY },
-        { 31, MUELLER_RATE_MULTIPLIER },
-        { 32, MUELLER_DENSITY_EXPONENT },
-        //@}
-
-        /// EventScheduler: v in "Case Fatality Rate proposal"
-        { 33, CFR_SCALE_FACTOR },
-
-        /// @b Molineaux: sampling parameters (not pairwise mode only)
-        { 34, MEAN_LOCAL_MAX_DENSITY },
-        { 35, SD_LOCAL_MAX_DENSITY },
-        { 36, MEAN_DIFF_POS_DAYS },
-        { 37, SD_DIFF_POS_DAYS },
-
-        /// EventScheduler: exp(-CFR_NEG_LOG_ALPHA) is the proportion of deaths occuring on the first day
-        { 38, CFR_NEG_LOG_ALPHA }
+    /*
+    * Defines the map from parameter names to parameter (optional) values.
+    *
+    * The keys of this map may not form a contiguous sequence, e.g. in the case where a
+    * parameter gets deleted altogether from the simulation code.  This is okay, and is
+    * in fact already the case at the time of writing.
+    *
+    * TODO : probably just intialize this in a loop or something...
+    */
+    std::unordered_map<ParameterName, std::optional<double>> nameToValueMap
+    {
+        { ParameterName::NEG_LOG_ONE_MINUS_SINF,       std::nullopt },
+        { ParameterName::E_STAR,                       std::nullopt },
+        { ParameterName::SIMM,                         std::nullopt },
+        { ParameterName::X_STAR_P,                     std::nullopt },
+        { ParameterName::GAMMA_P,                      std::nullopt },
+        { ParameterName::SIGMA_I_SQ,                   std::nullopt },
+        { ParameterName::CUMULATIVE_Y_STAR,            std::nullopt },
+        { ParameterName::CUMULATIVE_H_STAR,            std::nullopt },
+        { ParameterName::NEG_LOG_ONE_MINUS_ALPHA_M,    std::nullopt },
+        { ParameterName::DECAY_M,                      std::nullopt },
+        { ParameterName::SIGMA0_SQ,                    std::nullopt },
+        { ParameterName::X_NU_STAR,                    std::nullopt },
+        { ParameterName::Y_STAR_SQ,                    std::nullopt },
+        { ParameterName::ALPHA,                        std::nullopt },
+        { ParameterName::DENSITY_BIAS_NON_GARKI,       std::nullopt },
+        { ParameterName::BASELINE_AVAILABILITY_SHAPE,  std::nullopt },
+        { ParameterName::LOG_ODDS_RATIO_CF_COMMUNITY,  std::nullopt },
+        { ParameterName::INDIRECT_RISK_COFACTOR,       std::nullopt },
+        { ParameterName::NON_MALARIA_INFANT_MORTALITY, std::nullopt },
+        { ParameterName::DENSITY_BIAS_GARKI,           std::nullopt },
+        { ParameterName::SEVERE_MALARIA_THRESHHOLD,    std::nullopt },
+        { ParameterName::IMMUNITY_PENALTY,             std::nullopt },
+        { ParameterName::IMMUNE_EFFECTOR_DECAY,        std::nullopt },
+        { ParameterName::COMORBIDITY_INTERCEPT,        std::nullopt },
+        { ParameterName::Y_STAR_HALF_LIFE,             std::nullopt },
+        { ParameterName::Y_STAR_1,                     std::nullopt },
+        { ParameterName::ASEXUAL_IMMUNITY_DECAY,       std::nullopt },
+        { ParameterName::Y_STAR_0,                     std::nullopt },
+        { ParameterName::CRITICAL_AGE_FOR_COMORBIDITY, std::nullopt },
+        { ParameterName::MUELLER_RATE_MULTIPLIER,      std::nullopt },
+        { ParameterName::MUELLER_DENSITY_EXPONENT,     std::nullopt },
+        { ParameterName::CFR_SCALE_FACTOR,             std::nullopt },
+        { ParameterName::MEAN_LOCAL_MAX_DENSITY,       std::nullopt },
+        { ParameterName::SD_LOCAL_MAX_DENSITY,         std::nullopt },
+        { ParameterName::MEAN_DIFF_POS_DAYS,           std::nullopt },
+        { ParameterName::SD_DIFF_POS_DAYS,             std::nullopt },
+        { ParameterName::CFR_NEG_LOG_ALPHA,            std::nullopt },
     };
 };
 
